aa632b65cb128cd03dd7c1ad172308e5be895d9a
Stanislav Morozov <r3bfish@gmail.com>
2015-02-27 09:12:09 +0300

    1. Calendar mini 2. Add icons for calendar 3. Add invites

--- source/js/module/pages/events/events_invites.js
+++ source/js/module/pages/events/events_invites.js
@@ -2,9 +2,33 @@ var EventsView;
 
 EventsView = React.createClass({
 	mixins: [Morearty.Mixin],
-	render: function() {
-		return <div className="eEvents_pageContent mActiveEvents">
+	getInvites: function () {
+		var binding = this.getDefaultBinding(),
+			inviteCount = binding.get().count();
 
+		if (inviteCount > 0) {
+			return binding.get('models').map(function (invite) {
+				return <div className="eInvite">
+					<span className="eInvite_inviter">Inviter: {invite.get('inviterId')}</span>
+					<span className="eInvite_type">Type: {invite.get('invitedType')}</span>
+					<div className="eInvite_messages">Message: {invite.get('message')}</div>
+					<span className="eInvite_redeemed">redeemed: {invite.get('redeemed') ? 'redeemed' : 'not redeemed'}</span>
+					{invite.get('accepted') ?
+						<button className="eInvite_accepted bButton mDisable">Accepted</button> :
+						<button className="eInvite_accepted bButton mEnable">Accept</button>
+						}
+				</div>;
+			}).toArray();
+		} else {
+			return <div className="eNotFound">You haven't invites.</div>
+		}
+	}
+	,
+	render: function() {
+		var self = this;
+
+		return <div className="bInvites">
+		{self.getInvites()}
 		</div>;
 	}
 });
