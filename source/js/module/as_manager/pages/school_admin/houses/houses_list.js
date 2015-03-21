2f914dfc1472bd3e1634c28b90b88a19091206c9
Kind Wizzard <KindWizzard@users.noreply.github.com>
2015-03-22 01:02:23 +0300

    1. Folder rename

---
+++ source/js/module/as_manager/pages/school_admin/houses/houses_list.js
@@ -0,0 +1,25 @@
+var List = require('module/ui/list/list'),
+	ListField = require('module/ui/list/list_field'),
+	Table = require('module/ui/list/table'),
+	TableField = require('module/ui/list/table_field'),
+	ListPageMixin = require('module/as_manager/pages/school_admin/list_page_mixin'),
+	HousesListPage;
+
+HousesListPage = React.createClass({
+	mixins: [Morearty.Mixin, ListPageMixin],
+	serviceName: 'houses',
+	getTableView: function() {
+		var self = this,
+			binding = self.getDefaultBinding();
+
+		return (
+			<Table title="Houses" binding={binding} onItemEdit={self._getEditFunction()} onFilterChange={self.updateData}>
+				<TableField dataField="name" width="180px">House name</TableField>
+				<TableField dataField="description">Description</TableField>
+			</Table>
+		);
+	}
+});
+
+
+module.exports = HousesListPage;
---
+++ source/js/module/as_manager/pages/school_profile/school_profile_page.js
@@ -0,0 +1,103 @@
+var OneSchoolPage,
+	RouterView = require('module/core/router'),
+	Route = require('module/core/route'),
+	SubMenu = require('module/ui/menu/sub_menu'),
+	SVG = require('module/ui/svg'),
+	SchoolInfo = require('module/as_manager/pages/school_profile/view/school_info'),
+	UserButtons = require('module/as_manager/pages/school_profile/view/user_buttons'),
+	SchoolName = require('module/as_manager/pages/school_profile/view/school_name'),
+	Map = require('module/as_manager/pages/school_profile/view/map');
+
+OneSchoolPage = React.createClass({
+	mixins: [Morearty.Mixin],
+	componentWillMount: function() {
+		var self = this,
+			binding = self.getDefaultBinding(),
+			globalBinding = self.getMoreartyContext().getBinding(),
+			routerParameters = globalBinding.toJS('routing.pathParameters'),
+			activeSchoolId = routerParameters[0];
+
+		if (!activeSchoolId) {
+			document.location.hash = 'schools';
+		}
+
+		window.Server.school.get(activeSchoolId).then(function (data) {
+			binding.set('schoolInfo', Immutable.fromJS(data));
+		});
+
+		// Пункты подменю
+		self.menuItems = [{
+			href: '/#profile/summary',
+			name: 'Summary',
+			key: 'Summary'
+		},{
+			href: '/#profile/pupils',
+			name: 'Teams',
+			key: 'Teams'
+		},{
+			href: '/#profile/forms',
+			name: 'Forms',
+			key: 'Forms'
+		},{
+			href: '/#profile/houses',
+			name: 'Houses',
+			key: 'Houses'
+		}];
+	},
+
+	getDefaultState: function () {
+		return Immutable.fromJS({
+			classes: {
+				classesList: [],
+				classesRouting: {},
+				classesForm: {}
+			},
+			houses: {
+				housesList: [],
+				housesRouting: {},
+				housesForm: {}
+			},
+			pupils: {
+				pupilsList: [],
+				pupilsRouting: {},
+				pupilForm: {}
+			},
+			schoolInfo: '',
+			schoolRouting: {}
+		});
+	},
+	render: function() {
+		var self = this,
+			binding = self.getDefaultBinding(),
+			globalBinding = self.getMoreartyContext().getBinding();
+
+		return (
+			<div>
+				<div className="bUserColumn">
+					<Map binding={binding} />
+
+					<div className="eUserColumnData">
+						<SchoolName binding={binding} />
+						<SchoolInfo binding={binding} />
+					</div>
+				</div>
+
+				<div className="bUserDataColumn">
+					<SubMenu binding={binding.sub('schoolProfileRouting')} items={self.menuItems} />
+
+					<div className="eUserDataColumn_wrap" id="jsSubPage">
+
+
+						<UserButtons />
+
+
+
+					</div>
+				</div>
+			</div>
+		)
+	}
+});
+
+
+module.exports = OneSchoolPage;
