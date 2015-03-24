a3d4b6a56be152a9e5f3db9fab9ca3ebbf2afa2e
Kind Wizzard <KindWizzard@users.noreply.github.com>
2015-03-25 02:29:58 +0300

    2. Git bag

---
+++ source/js/module/ui/form/types/text.js
@@ -0,0 +1,49 @@
+var TypeMixin = require('module/ui/form/types/type_mixin'),
+	TypeText;
+
+TypeText =  React.createClass({
+	mixins: [Morearty.Mixin, TypeMixin],
+	componentWillMount: function() {
+		var self = this,
+			binding = self.getDefaultBinding();
+
+		// На случай, если форма заполняется асинхронно
+		binding.addListener('defaultValue', function() {
+			self._forceNewValue(binding.get('defaultValue'));
+		});
+	},
+	_forceNewValue: function(value) {
+		var self = this,
+			oldValue;
+
+		if (value !== undefined && self.refs.fieldInput && self.refs.fieldInput.getDOMNode().value === '') {
+			self.refs.fieldInput.getDOMNode().value = value;
+			self.fullValidate(value);
+		}
+	},
+	handeBlur: function(event) {
+		var self = this;
+
+		self.setValue(self.refs.fieldInput.getDOMNode().value);
+	},
+	handleChange: function(event) {
+		var self = this;
+
+		self.changeValue(self.refs.fieldInput.getDOMNode().value);
+	},
+	render: function () {
+		var self = this,
+			defaultValue = self.getDefaultBinding().get('defaultValue');
+
+		self._forceNewValue(defaultValue);
+
+		return (
+			<div className="eForm_fieldInput">
+				<input ref="fieldInput" type="text" onBlur={self.handeBlur} onChange={self.handleChange} />
+			</div>
+		)
+	}
+});
+
+
+module.exports = TypeText;
