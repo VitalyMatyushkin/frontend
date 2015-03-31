13aa40c1bae768501e3e6ab580ca7aeef23126d7
Stanislav Morozov <r3bfish@gmail.com>
2015-04-01 00:29:29 +0300

    1. add extra information on calendar part 2. add styles for challenge_list on calendar page

--- source/js/module/as_manager/pages/events/calendar/challenges_list.js
+++ source/js/module/as_manager/pages/events/calendar/challenges_list.js
@@ -3,9 +3,43 @@ var ChallengesList,
 
 ChallengesList = React.createClass({
     mixins: [Morearty.Mixin, InvitesMixin],
+    getRivalName: function(event, order) {
+        var self = this,
+            binding = self.getDefaultBinding(),
+            eventIndex = binding.get('models').findIndex(function (model) {
+                return model.get('id') === event.get('id');
+            }),
+            eventBinding = binding.sub(['models', eventIndex]),
+            type = event.get('type'),
+            played = !!event.get('resultId'),
+            rivalName = null,
+            eventResult = event.get('result').toJS();
+
+        if (type === 'internal') {
+            rivalName = eventBinding.get(['participants', order, 'name']);
+            if (played && rivalName) {
+                rivalName += '(' + eventResult.summary.byTeams[eventBinding.get(['participants', order, 'id'])] + ')';
+            }
+        } else if (type === 'houses') {
+            rivalName = eventBinding.get(['participants', order, 'house', 'name']);
+            if (played && rivalName) {
+                rivalName += '(' + eventResult.summary.byTeams[eventBinding.get(['participants', order, 'house', 'id'])] + ')';
+            }
+        } else {
+            rivalName = eventBinding.get(['participants', order, 'school', 'name']);
+
+            if (played && rivalName) {
+                rivalName += '(' + eventResult.summary.byTeams[eventBinding.get(['participants', order, 'school', 'id'])] + ')';
+            } else if (!rivalName) {
+                rivalName = eventBinding.get(['invites', 0, 'guest', 'name']);
+            }
+        }
+
+        return rivalName;
+    },
     getEvents: function () {
         var self = this,
-            binding = this.getDefaultBinding(),
+            binding = self.getDefaultBinding(),
             currentDate = binding.get('calendar.currentDate'),
             events = binding.get('models').filter(function (event) {
                 var eventDate = new Date(event.get('startTime'));
@@ -23,8 +57,15 @@ ChallengesList = React.createClass({
                 stringDate = self.formatDate(event.get('startTime'));
 
             return <div className={isHoverDay ? 'eChallenge mActive' : 'eChallenge'}>
-                <span className="eChallenge_date">{stringDate}</span>
-                <span className="eChallenge_name">{event.get('name')}</span>
+                <div className="eChallenge_basic">
+                    <span className="eChallenge_date">{stringDate}</span>
+                    <span className="eChallenge_type">{event.get('type')}</span>
+                </div>
+                <div className="eChallenge_name">{event.get('name')}</div>
+                <div className="eChallenge_rivals">
+                    <span className="eChallenge_rivalName">{self.getRivalName(event, 0)}</span>
+                    <span className="eChallenge_rivalName">{self.getRivalName(event, 1)}</span>
+                </div>
             </div>
         }).toArray() : <div className="eChallenge mNotFound">You haven't events on this month.</div>;
     },
