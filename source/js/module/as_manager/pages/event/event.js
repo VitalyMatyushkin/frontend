ddca28bbd15493760ea2476a90beab66c52c38a6
Stanislav Morozov <r3bfish@gmail.com>
2015-05-10 14:51:50 +0600

    add album from event

--- source/js/module/as_manager/pages/event/event.js
+++ source/js/module/as_manager/pages/event/event.js
@@ -2,6 +2,7 @@ var EventView,
     RouterView = require('module/core/router'),
     Route = require('module/core/route'),
     If = require('module/ui/if/if'),
+    EventAlbums = require('./view/event_album'),
     EventHeader = require('./view/event_header'),
     EventRivals = require('./view/event_rivals'),
     EventButtons = require('./view/event_buttons'),
@@ -100,6 +101,9 @@ EventView = React.createClass({
                     },
                     {
                         sport: ''
+                    },
+                    {
+                        albums: ''
                     }
                 ]
             }
@@ -109,6 +113,7 @@ EventView = React.createClass({
 				invites = res.invites,
 				activeSchoolId = rootBinding.get('userRules.activeSchoolId'),
 				sport = res.sport,
+                albums = res.albums,
 				schoolInfo = event.participants[0].school.id === activeSchoolId ?
 					event.participants[0].school : event.participants[1].school,
                 points = event.result ? event.result.points : [];
@@ -130,6 +135,7 @@ EventView = React.createClass({
 				]))
 				.set('schoolInfo', Immutable.fromJS(schoolInfo))
 				.set('eventId', eventId)
+                .set('albums', Immutable.fromJS(albums))
                 .set('mode', 'general')
                 .set('sync', true)
 				.commit();
@@ -151,6 +157,7 @@ EventView = React.createClass({
                         <EventButtons binding={binding} />
                         <EventHeader binding={binding} />
                         <EventRivals binding={binding} />
+                        <EventAlbums binding={binding} />
                         <EventTeams binding={binding} />
                     </div>
                 </If>
