eebe1fbea5ed8435473a75ca72201d2fd783dbac
Kind Wizzard <KindWizzard@users.noreply.github.com>
2015-05-13 20:08:25 +0300

    1. Opponents page

--- source/js/module/as_main/pages/opponents/list/list_page.js
+++ source/js/module/as_main/pages/opponents/list/list_page.js
@@ -1,81 +1,23 @@
-var OpponentsListPage;
+var List = require('module/ui/list/list'),
+	ListField = require('module/ui/list/list_field'),
+	Table = require('module/ui/list/table'),
+	TableField = require('module/ui/list/table_field'),
+	DateTimeMixin = require('module/mixins/datetime'),
+	ListPageMixin = require('module/as_main/pages/opponents/list/list_page_mixin'),
+	OpponentsListPage;
 
 OpponentsListPage = React.createClass({
-	mixins: [Morearty.Mixin],
-	render: function() {
+	mixins: [Morearty.Mixin, ListPageMixin, DateTimeMixin],
+	getTableView: function() {
 		var self = this,
 			binding = self.getDefaultBinding();
-
+		//schoolOpponents
 		return (
-			<div classNameName="bOpponentsList">
-
-				<div className="bSchoolMaster">
-					<div className="bFiltersPage mNoFilters">
-						<h1 className="eSchoolMaster_title"><span>Opponents list</span>
-
-							<div className="eSchoolMaster_buttons">
-								<div className="bButton"><span>Filters </span><span>⇣</span></div>
-							</div>
-						</h1>
-						<div className="bDataList">
-							<div className="eDataList_list mTable">
-								<div className="eDataList_listItem mHead">
-									<div className="eDataList_listItemCell" style={{width: '35%'}}><span>School name</span>
-									</div>
-									<div className="eDataList_listItemCell"style={{width: '27%'}}><span>Adress</span></div>
-									<div className="eDataList_listItemCell"style={{width: '18%'}}><span>Phone</span></div>
-									<div className="eDataList_listItemCell mActions">Actions</div>
-								</div>
-								<div className="eDataList_listItem">
-									<div className="eDataList_listItemCell">London School of Hygiene &amp; Tropical Medicine</div>
-									<div className="eDataList_listItemCell">The Ridgeway, Mill Hill Village</div>
-									<div className="eDataList_listItemCell">845 320 654 98</div>
-									<div className="eDataList_listItemCell mActions"><span className="bLinkLike">Open fixtures</span></div>
-								</div>
-								<div className="eDataList_listItem">
-									<div className="eDataList_listItemCell">Barnet College</div>
-									<div className="eDataList_listItemCell">Keppel Street, London WC1E 7HT</div>
-									<div className="eDataList_listItemCell">845 700 654 53</div>
-									<div className="eDataList_listItemCell mActions"><span className="bLinkLike">Open fixtures</span></div>
-								</div>
-								<div className="eDataList_listItem">
-									<div className="eDataList_listItemCell">Belmont School</div>
-									<div className="eDataList_listItemCell">658 Romford Road</div>
-									<div className="eDataList_listItemCell">845 257 600 24</div>
-									<div className="eDataList_listItemCell mActions"><span className="bLinkLike">Open fixtures</span></div>
-								</div>
-								<div className="eDataList_listItem">
-									<div className="eDataList_listItemCell">London School of Hygiene &amp; Tropical Medicine</div>
-									<div className="eDataList_listItemCell">Keppel Street, London WC1E 7HT</div>
-									<div className="eDataList_listItemCell">845 421 654 15</div>
-									<div className="eDataList_listItemCell mActions"><span className="bLinkLike">Open fixtures</span></div>
-								</div>
-								<div className="eDataList_listItem">
-									<div className="eDataList_listItemCell">American InterContinental University London</div>
-									<div className="eDataList_listItemCell">Bownley Rd</div>
-									<div className="eDataList_listItemCell">845 245 856 57</div>
-									<div className="eDataList_listItemCell mActions"><span className="bLinkLike">Open fixtures</span></div>
-								</div>
-								<div className="eDataList_listItem">
-									<div className="eDataList_listItemCell">Alleyn's School</div>
-									<div className="eDataList_listItemCell">Keppel Street, London WC1E 7HT</div>
-									<div className="eDataList_listItemCell">845 123 75 22</div>
-									<div className="eDataList_listItemCell mActions"><span className="bLinkLike">Open fixtures</span></div>
-								</div>
-								<div className="eDataList_listItem">
-									<div className="eDataList_listItemCell">Barnet College</div>
-									<div className="eDataList_listItemCell">110 Marylebone High St</div>
-									<div className="eDataList_listItemCell">245 18 87 67</div>
-									<div className="eDataList_listItemCell mActions"><span className="bLinkLike">Open fixtures</span></div>
-								</div>
-
-							</div>
-
-						</div>
-					</div>
-				</div>
-
-			</div>
+			<Table title="Opponents list" binding={binding} onFilterChange={self.updateData}>
+				<TableField width="30%" dataField="firstName">School name</TableField>
+				<TableField width="25%" dataField="firstName">Adress</TableField>
+				<TableField width="20%" dataField="firstName">Phone</TableField>
+			</Table>
 		)
 	}
 });
--- source/js/module/core/service_list.js
+++ source/js/module/core/service_list.js
@@ -1,84 +1,86 @@
 var Service = require('module/core/service'),
-    serviceList,
-    binding;
+	serviceList,
+	binding;
 
 
 serviceList = {
-    initialize: function (binding) {
-        serviceList.me = new Service('/users/{ownerId}', binding);
-        serviceList.logout = new Service('/users/logout', binding);
+	initialize: function(binding) {
+		serviceList.me = new Service('/users/{ownerId}', binding);
+		serviceList.logout = new Service('/users/logout', binding);
 
-        // users
-        serviceList.users = new Service('/users', binding);
-        serviceList.user = new Service('/users/{id}', binding);
-        serviceList.userChildren = new Service('/users/{id}/children', binding);
+		// users
+		serviceList.users = new Service('/users', binding);
+		serviceList.user = new Service('/users/{id}', binding);
+		serviceList.userChildren = new Service('/users/{id}/children', binding);
 
-        // schools
-        serviceList.schools = new Service('/schools', binding);
-        serviceList.schoolsFindOne = new Service('/schools/findOne', binding);
-        serviceList.school = new Service('/schools/{id}', binding);
-        serviceList.schoolInfo = new Service('/schools/findOne?filter[where][id]={id}&filter[include]=zipCode', binding);
+		// schools
+		serviceList.schools = new Service('/schools', binding);
+		serviceList.schoolsFindOne = new Service('/schools/findOne', binding);
+		serviceList.school = new Service('/schools/{id}', binding);
+		serviceList.schoolInfo = new Service('/schools/findOne?filter[where][id]={id}&filter[include]=zipCode', binding);
 
-        serviceList.fixturesBySchoolId = new Service('/schools/{schoolId}/public/events', binding);
-        serviceList.eventsBySchoolId = new Service('/schools/{schoolId}/events', binding);
-        serviceList.ownerSchools = new Service('/schools?filter[where][ownerId]={ownerId}', binding);
-        serviceList.schoolCoaches = new Service('/schools/{id}/coaches', binding);
+		serviceList.fixturesBySchoolId = new Service('/schools/{schoolId}/public/events', binding);
+		serviceList.eventsBySchoolId = new Service('/schools/{schoolId}/events', binding);
+		serviceList.ownerSchools = new Service('/schools?filter[where][ownerId]={ownerId}', binding);
+		serviceList.schoolCoaches = new Service('/schools/{id}/coaches', binding);
+		serviceList.schoolOpponents = new Service('/schools/{id}/public/opponents', binding);
 
-        // students
-        serviceList.students = new Service('/schools/{schoolId}/students', binding);
+
+		// students
+		serviceList.students = new Service('/schools/{schoolId}/students', binding);
 		serviceList.student = new Service('/students/{studentId}', binding);
-		serviceList.studentGamesWon = new Service('/students/{id}/events/won?include={include}',binding);
-        serviceList.studentGamesScored = new Service('/students/{id}/events/scored?include={include}',binding);
-		serviceList.studentEvents = new Service('/students/{id}/events',binding);
+		serviceList.studentGamesWon = new Service('/students/{id}/events/won?include={include}', binding);
+		serviceList.studentGamesScored = new Service('/students/{id}/events/scored?include={include}', binding);
+		serviceList.studentEvents = new Service('/students/{id}/events', binding);
 
-        // houses
-        serviceList.houses = new Service('/schools/{schoolId}/houses', binding);
-        serviceList.house = new Service('/houses/{houseId}', binding);
+		// houses
+		serviceList.houses = new Service('/schools/{schoolId}/houses', binding);
+		serviceList.house = new Service('/houses/{houseId}', binding);
 
-        // forms
-        serviceList.forms = new Service('/schools/{schoolId}/forms', binding);
-        serviceList.form = new Service('/forms/{formId}', binding);
+		// forms
+		serviceList.forms = new Service('/schools/{schoolId}/forms', binding);
+		serviceList.form = new Service('/forms/{formId}', binding);
 
-        // news
-        serviceList.news = new Service('/schools/{schoolId}/news', binding);
-        serviceList.oneNews = new Service('/news/{formId}', binding);
+		// news
+		serviceList.news = new Service('/schools/{schoolId}/news', binding);
+		serviceList.oneNews = new Service('/news/{formId}', binding);
 
-        //events
-        serviceList.events = new Service('/events', binding);
-        serviceList.eventFindOne = new Service('/events/findOne', binding);
-        serviceList.event = new Service('/events/{eventId}', binding);
-        serviceList.participants = new Service('/events/{eventId}/participants', binding);
+		//events
+		serviceList.events = new Service('/events', binding);
+		serviceList.eventFindOne = new Service('/events/findOne', binding);
+		serviceList.event = new Service('/events/{eventId}', binding);
+		serviceList.participants = new Service('/events/{eventId}/participants', binding);
 
-        // sports
-        serviceList.sports = new Service('/sports', binding);
+		// sports
+		serviceList.sports = new Service('/sports', binding);
 
-        // invites
-        serviceList.invites = new Service('/invites', binding);
-        serviceList.invitesFindOne = new Service('/invites/findOne', binding);
-        serviceList.invite = new Service('/invites/{inviteId}', binding);
-        serviceList.invitesByEvent = new Service('/events/{eventId}/invites', binding);
-        serviceList.inviteRepay = new Service('/invites/{inviteId}/repay', binding);
+		// invites
+		serviceList.invites = new Service('/invites', binding);
+		serviceList.invitesFindOne = new Service('/invites/findOne', binding);
+		serviceList.invite = new Service('/invites/{inviteId}', binding);
+		serviceList.invitesByEvent = new Service('/events/{eventId}/invites', binding);
+		serviceList.inviteRepay = new Service('/invites/{inviteId}/repay', binding);
 
-        // teams
-        serviceList.teamsBySchoolId = new Service('/teams?filter[where][schoolId]={schoolId}&filter[include]=events', binding);
-        serviceList.playersRelation = new Service('/teams/{teamId}/players/rel/{studentId}', binding);
-        serviceList.playersByTeam = new Service('/teams/{teamId}/players', binding);
+		// teams
+		serviceList.teamsBySchoolId = new Service('/teams?filter[where][schoolId]={schoolId}&filter[include]=events', binding);
+		serviceList.playersRelation = new Service('/teams/{teamId}/players/rel/{studentId}', binding);
+		serviceList.playersByTeam = new Service('/teams/{teamId}/players', binding);
 
-        // result
-        serviceList.results = new Service('/results', binding);
+		// result
+		serviceList.results = new Service('/results', binding);
 		serviceList.result = new Service('/results/{id}', binding);
-		serviceList.resultByEvent = new Service('/results/{id}/event',binding);
+		serviceList.resultByEvent = new Service('/results/{id}/event', binding);
 
-        // points
-        serviceList.pointsInResult = new Service('/results/{resultId}/points', binding);
-        serviceList.studentPoints = new Service('/points?filter[where][studentId]={studentId}', binding);
-        serviceList.points = new Service('/points', binding);
+		// points
+		serviceList.pointsInResult = new Service('/results/{resultId}/points', binding);
+		serviceList.studentPoints = new Service('/points?filter[where][studentId]={studentId}', binding);
+		serviceList.points = new Service('/points', binding);
 
-        // postcode
-        serviceList.postCode = new Service('/zipcodes', binding);
-        serviceList.findPostCode = new Service('/zipcodes/findOne?filter[where][zipCode]={postCode}', binding);
-        serviceList.findPostCodeById = new Service('/zipcodes/findOne?filter[where][id]={postCode}', binding);
-    }
+		// postcode
+		serviceList.postCode = new Service('/zipcodes', binding);
+		serviceList.findPostCode = new Service('/zipcodes/findOne?filter[where][zipCode]={postCode}', binding);
+		serviceList.findPostCodeById = new Service('/zipcodes/findOne?filter[where][id]={postCode}', binding);
+	}
 };
 
 
