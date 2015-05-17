16159cea7b6a04ac68e994236c07454a0774a5b7
Stanislav Morozov <r3bfish@gmail.com>
2015-05-17 21:26:18 +0300

    add home page link

--- source/js/module/as_manager/pages/school_admin/summary/summary_page.js
+++ source/js/module/as_manager/pages/school_admin/summary/summary_page.js
@@ -1,54 +1,59 @@
 var SchoolSummary,
-	SVG = require('module/ui/svg');
+    SVG = require('module/ui/svg');
 
 SchoolSummary = React.createClass({
-	mixins: [Morearty.Mixin],
-	componentWillMount: function () {
-		var self = this,
-			binding = self.getDefaultBinding(),
-			globalBinding = self.getMoreartyContext().getBinding(),
-			activeSchoolId = globalBinding.get('userRules.activeSchoolId');
+    mixins: [Morearty.Mixin],
+    componentWillMount: function() {
+        var self = this,
+            binding = self.getDefaultBinding(),
+            globalBinding = self.getMoreartyContext().getBinding(),
+            activeSchoolId = globalBinding.get('userRules.activeSchoolId');
 
-		self.activeSchoolId = activeSchoolId;
-		self.request = window.Server.schoolsFindOne.get({
+        self.activeSchoolId = activeSchoolId;
+        self.request = window.Server.schoolsFindOne.get({
             filter: {
                 where: {
                     id: activeSchoolId
                 }
             }
-        }).then(function (data) {
-			binding.set(Immutable.fromJS(data));
-			self.isMounted() && self.forceUpdate();
-		});
-	},
-	componentWillUnmount: function () {
-		var self = this;
+        }).then(function(data) {
+            binding.set(Immutable.fromJS(data));
+            self.isMounted() && self.forceUpdate();
+        });
+    },
+    componentWillUnmount: function() {
+        var self = this;
 
-		self.request && self.request.abort();
-	},
-	render: function () {
-		var self = this,
-			binding = self.getDefaultBinding(),
-			schoolPicture = binding.get('pic');
+        self.request && self.request.abort();
+    },
+    render: function() {
+        var self = this,
+            binding = self.getDefaultBinding(),
+            schoolPicture = binding.get('pic'),
+            siteLink = binding.get('domain') + '.squadintouch.com';
 
-		return (
-			<div>
-				<h1 className="eSchoolMaster_title">
-					{schoolPicture ? <div className="eSchoolMaster_flag"><img src={schoolPicture}/> </div> : ''}
-					{binding.get('name')}
+        return (
+            <div>
+                <h1 className="eSchoolMaster_title">
+                    {schoolPicture ? <div className="eSchoolMaster_flag"><img src={schoolPicture}/></div> : ''}
+                    {binding.get('name')}
 
-					<div className="eSchoolMaster_buttons">
+                    <div className="eSchoolMaster_buttons">
 
-						<a href={'/#schools/edit?id=' + self.activeSchoolId} className="bButton">Edit...</a>
-						<a href="/#schools" className="bButton">Change active school...</a>
-					</div>
-				</h1>
+                        <a href={'/#schools/edit?id=' + self.activeSchoolId} className="bButton">Edit...</a>
+                        <a href="/#schools" className="bButton">Change active school...</a>
+                    </div>
+                </h1>
                 <p>PostCode: {binding.get('postcodeId')}</p>
-				<p>Address: {binding.get('address')}</p>
-				<p>Description: {binding.get('description')}</p>
-			</div>
-		)
-	}
+
+                <p>Address: {binding.get('address')}</p>
+
+                <p>Description: {binding.get('description')}</p>
+
+                <p>Site: <a href={'//' + siteLink} target="blank" title="binding.get('name') homepage">http://{siteLink}</a></p>
+            </div>
+        )
+    }
 });
 
 
