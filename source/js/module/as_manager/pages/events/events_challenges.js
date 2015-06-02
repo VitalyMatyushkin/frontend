8882dc444a1cb1ebb96cdc05b977a2b507480904
Stanislav Morozov <r3bfish@gmail.com>
2015-06-02 18:24:41 +0300

    #135 fixed

--- source/js/module/as_manager/pages/events/events_challenges.js
+++ source/js/module/as_manager/pages/events/events_challenges.js
@@ -46,13 +46,10 @@ ChallengesView = React.createClass({
                 firstPoint,
                 secondPoint;
             if (type === 'inter-schools') {
-				//http://i.imgur.com/9br7NSU.jpg
-                firstName = eventBinding.get('participants.0.school.name');
-                secondName = !binding.get('model.resultId') ? eventBinding.get('invites.0.guest.name') : eventBinding.get('participants.1.school.name');
-
-				firstPic = eventBinding.get('participants.0.school.pic');
-				secondPic = eventBinding.get('participants.1.school.pic') || eventBinding.get('invites.0.guest.pic');
-
+                firstName = eventBinding.get('invites.0.guest.name');
+                firstPic = eventBinding.get('invites.0.guest.pic');
+                secondName = eventBinding.get('invites.0.inviter.name');
+                secondPic = eventBinding.get('invites.0.inviter.pic');
             } else if (type === 'houses') {
                 firstName = eventBinding.get('participants.0.house.name');
                 secondName = eventBinding.get('participants.1.house.name');
