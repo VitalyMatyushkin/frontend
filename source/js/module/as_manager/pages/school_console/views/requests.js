9eb3aa5865f013587d44c050fc64175a5b2f1885
brightboahen <bridark17@gmail.com>
2015-06-24 18:33:48 +0100

    #175 requests page

--- source/js/module/as_manager/pages/school_console/views/requests.js
+++ source/js/module/as_manager/pages/school_console/views/requests.js
@@ -1,3 +1,13 @@
 /**
  * Created by bridark on 19/06/15.
  */
+var SchoolRequest;
+SchoolRequest = React.createClass({
+    mixins:[Morearty.Mixin],
+    render:function(){
+        return(
+            <div>Live requests go here</div>
+        )
+    }
+});
+module.exports = SchoolRequest;
