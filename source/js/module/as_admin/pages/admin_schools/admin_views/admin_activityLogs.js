feb55aedd09f0327a2454e607048d2851cfcb19a
brightboahen <bridark17@gmail.com>
2015-07-20 12:07:23 +0100

    #175 - activity log interface

--- source/js/module/as_admin/pages/admin_schools/admin_views/admin_activityLogs.js
+++ source/js/module/as_admin/pages/admin_schools/admin_views/admin_activityLogs.js
@@ -4,9 +4,10 @@
 var ActivityLogPage,
     SVG = require('module/ui/svg'),
     UserLogs = require('../admin_comps/user_logs'),
-    LogPagination = require('../admin_comps/log_pagination');
+    LogPagination = require('../admin_comps/log_pagination'),
+    DateTimeMixin = require('module/mixins/datetime');
 ActivityLogPage = React.createClass({
-    mixins:[Morearty.Mixin],
+    mixins:[Morearty.Mixin,DateTimeMixin],
     getDefaultState: function () {
         return Immutable.fromJS({
             logs:[]
@@ -16,6 +17,38 @@ ActivityLogPage = React.createClass({
         var self = this,
             binding = self.getDefaultBinding();
         window.Server.activityLogs.get({filter:{limit:40}}).then(function (res) {
+            binding
+                .atomically()
+                .set('logs',Immutable.fromJS(res))
+                .commit();
+            binding.set('filterMode','').set('filterActive',false);
+        });
+    },
+    _handleFiltering:function(filter){
+        var self = this,
+            binding = self.getDefaultBinding(),
+            orderString = '';
+        switch (filter){
+            case 1:
+                orderString = 'meta.created DESC';
+                binding.set('filterMode',orderString).set('filterActive',true);
+                break;
+            case 2:
+                orderString = 'meta.created ASC';
+                binding.set('filterMode',orderString).set('filterActive',true);
+                break;
+            default :
+                //orderString = 'meta.created ASC';
+                //binding.set('filterMode',orderString).set('filterActive',true);
+                orderString = '';
+                break;
+        }
+        window.Server.activityLogs.get({
+            filter:{
+                order:orderString,
+                limit:40
+            }
+        }).then(function(res){
             binding
                 .atomically()
                 .set('logs',Immutable.fromJS(res))
@@ -31,6 +64,12 @@ ActivityLogPage = React.createClass({
                 <div className="eLogDataList_header">
                     <span><SVG icon="icon_stats-dots"/> ACTIVITY LOGS</span>
                 </div>
+                <div className="eLogDataList_filtering">
+                    <em>Filter by :</em>
+                    <span className={binding.get('filterMode') === 'meta.created DESC'? 'perActive':'perInActive'} onClick={function(){self._handleFiltering(1)}}>Latest</span>
+                    <span> | </span>
+                    <span className={binding.get('filterMode') === 'meta.created ASC'? 'perActive':'perInActive'} onClick={function(){self._handleFiltering(2)}}>Oldest</span>
+                </div>
                 <table>
                     <thead>
                         <tr>
@@ -40,7 +79,7 @@ ActivityLogPage = React.createClass({
                     </thead>
                     <UserLogs logData={logData} binding={binding} />
                 </table>
-                <LogPagination binding={binding} />
+                <LogPagination filterActive={binding.get('filterActive')} filterMode={binding.get('filterMode')} binding={binding} />
             </div>
         );
     }
