4b1150b25fe889ae86ef0e7ee38efa7d0cd5c09c
brightboahen <bridark17@gmail.com>
2015-07-28 18:17:04 +0100

    rewrote school list view for superadmin

--- source/js/module/as_admin/pages/admin_schools/admin_views/admin_list.js
+++ source/js/module/as_admin/pages/admin_schools/admin_views/admin_list.js
@@ -12,102 +12,49 @@ var List = require('module/ui/list/list'),
     theList;
 
 SchoolListPage = React.createClass({
-    mixins:[Morearty.Mixin],
-    componentWillMount:function(){
-        var self = this,
-            binding = self.getDefaultBinding();
-        window.Server.schools.get().then(function(data){
-             binding.set('schoolList',Immutable.fromJS(data));
-            self._updateListData(binding.get('schoolList').toJS());
-        });
+    mixins:[Morearty.Mixin,ListPageMixin,DateTimeMixin],
+    serviceName:'schools',
+    getSchoolLogo:function(logo){
+        if(logo){
+            return (
+                <span className="eChallenge_rivalPic">
+                    <img src={logo}/>
+                </span>
+            )
+        }
     },
-    _updateListData:function(listData){
+    getSchoolStatus:function(){
+
+    },
+    _getItemRemoveFunction:function(model){
         var self = this,
             binding = self.getDefaultBinding(),
-            deleteSchool = function(value){
-                return function (event){
-                    var confirmDelete = confirm("Do you want to delete school ?");
-                    if(confirmDelete){
-                        window.Server.school.delete({id:value}).then(function(result){
-                                window.Server.schools.get().then(function(data){
-                                    binding.set('schoolList',Immutable.fromJS(data));
-                                    self._updateListData(binding.get('schoolList').toJS());
-                                });
-                            }
-                        );
-                    }
-                    event.stopPropagation();
+            confirm = window.confirm("Do you really want to remove this item?");
+        if(confirm === true){
+            window.Server.school.delete({id:model.id}).then(function(){
+                    binding.update(function(result) {
+                        return result.filter(function(res) {
+                            return res.get('id') !== model.id;
+                        });
+                    });
                 }
-            };
-        theList = listData.map(function(school){
-            return(
-                <div className="eDataList_listItem" onClick={self.onSchoolClick.bind(null,school.id)}>
-                    <div className="eDataList_listItemCell"><span className="eChallenge_rivalPic"><img src={school.pic}/></span></div>
-                    <div className="eDataList_listItemCell">{school.name}</div>
-                    <div className="eDataList_listItemCell">{school.phone}</div>
-                    <div className="eDataList_listItemCell">{school.address}</div>
-                    <div className="eDataList_listItemCell">{school.domain}</div>
-                    <div className="eDataList_listItemCell">{typeof school.status === 'undefined'? 'N/A': school.status}</div>
-                    <div className="eDataList_listItemCell mActions">
-                        <span  onClick={deleteSchool(school.id)} className="bLinkLike">Delete</span>
-                    </div>
-                </div>
-            )
-        });
+            );
+        }
     },
-    onSchoolClick:function(value){
-        console.log('click'+value);
-        document.location.hash = '/admin_schools/admin_views/detail?id='+value
-    },
-    onChange:function(event){
-        var self = this,
-            binding = self.getDefaultBinding();
-            window.Server.schools.get({
-                filter: {
-                    where: {
-                        name: {
-                            like: event.currentTarget.value,
-                            options: 'i'
-                        }
-                    }
-                }
-            }).then(function(filtered){
-                binding.set('schoolList',Immutable.fromJS(filtered));
-                self._updateListData(binding.get('schoolList').toJS());
-            });
-    },
-    render:function(){
+    getTableView:function(){
         var self = this,
             binding = self.getDefaultBinding();
         return (
-           <div>
-               <h1 className="eSchoolMaster_title">
-                   <span>List of Schools on SquadInTouch</span>
-                   <div className="eSchoolMaster_buttons">
-                       <div className="eDataList_listItemCell">
-                           <div className="eDataList_filter">
-                               <input className="eDataList_filterInput" onChange={self.onChange}  placeholder={'filter by school name'} />
-                           </div>
-                       </div>
-                   </div>
-               </h1>
-               <div className="bDataList">
-                   <div className="eDataList_list mTable">
-                       <div className="eDataList_listItem mHead">
-                           <div className="eDataList_listItemCell" style={{width:3+'%'}}>Logo</div>
-                           <div className="eDataList_listItemCell" style={{width:20+'%'}}>
-                               Name
-                           </div>
-                           <div className="eDataList_listItemCell" style={{width:15+'%'}}>Telephone</div>
-                           <div className="eDataList_listItemCell" style={{width:37+'%'}}>Address</div>
-                           <div className="eDataList_listItemCell" style={{width:20+'%'}}>Domain</div>
-                           <div className="eDataList_listItemCell" style={{width:10+'%'}}>Status</div>
-                           <div className="eDataList_listItemCell" style={{width:10+'%'}}>Actions</div>
-                       </div>
-                       {theList}
-                   </div>
-               </div>
-           </div>
+            <div className="eTable_view">
+                <Table title="Schools" binding={binding} onItemRemove={self._getItemRemoveFunction} onFilterChange={self.updateData}>
+                    <TableField dataField="pic" width="1%" filterType="none" parseFunction={self.getSchoolLogo}>Logo</TableField>
+                    <TableField dataField="name" width="14%">School Name</TableField>
+                    <TableField dataField="phone" width="10%">Telephone</TableField>
+                    <TableField dataField="address" width="20%">Address</TableField>
+                    <TableField dataField="domain" width="14%">Domain</TableField>
+                    <TableField dataField="status" width="14%">Status</TableField>
+                </Table>
+            </div>
         )
     }
 });
