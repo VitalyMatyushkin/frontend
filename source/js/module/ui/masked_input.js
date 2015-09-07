1201d3e81b1affe3dddc591543dfc79b0f8df881
brightboahen <bridark17@gmail.com>
2015-09-07 14:35:44 +0100

    fix for issue #16 phone field not user friendly

--- source/js/module/ui/masked_input.js
+++ source/js/module/ui/masked_input.js
@@ -44,23 +44,24 @@ var MaskedInput = React.createClass({
 			if (~MASK_CHARS.indexOf(c)) {
 				rexps[i + 1] = MASK_REGEX[c]
 			}
-		})
+		});
 
-		var cursorMax = 0
-		var cursorMin = 0
-		var newValue = ''
-		var nextChar
+		var cursorMax = 0;
+		var cursorMin = 0;
+		var newValue = '';
+		var nextChar;
+        var tmpCur;
 
 		for (i = 0; i < mask.length; i++) {
 			if (~MASK_CHARS.indexOf(mask[i])) {
-				cursorMin = i
+				cursorMin = i;
 				break
 			}
 		}
 
 		for (var i = 0, j = 0; i < mask.length;) {
 			if (!~MASK_CHARS.indexOf(mask[i])) {
-				newValue += mask[i]
+				newValue += mask[i];
 				if (mask[i] == value[j]) {
 					j++
 				}
@@ -68,30 +69,34 @@ var MaskedInput = React.createClass({
 			} else {
 				if (nextChar = value.substr(j++, 1)) {
 					if (rexps[newValue.length + 1].test(nextChar)) {
-						newValue += nextChar
-						cursorMax = newValue.length
+						newValue += nextChar;
+						cursorMax = newValue.length;
+                        tmpCur = cursorMax;
 						i++
 					}
 				} else {
-					newValue = newValue.substr(0, cursorMax)
-					newValue += pattern.slice(cursorMax)
+					newValue = newValue.substr(0, cursorMax);
+					newValue += pattern.slice(cursorMax);
 					break
 				}
 			}
 		}
 
-		var cursorPrev = this.mask.cursor
-		var cursorCurr = this.isMounted() ? this.getDOMNode().selectionStart : 0
-		var removing = this.mask.cursor > cursorCurr
-		cursorMax = Math.max(cursorMax, cursorMin)
-
+		var cursorPrev = this.mask.cursor;
+		var cursorCurr = this.isMounted() ? this.getDOMNode().selectionStart : 0;
+		var removing = this.mask.cursor > cursorCurr;
+		cursorMax = Math.max(cursorMax, cursorMin);
 		if (cursorCurr <= cursorMin) {
-			cursorCurr = cursorMin
+            if(tmpCur === 2){
+                cursorCurr = tmpCur
+            }else{
+                cursorCurr = cursorMin
+            }
 		} else if (cursorCurr >= cursorMax) {
 			cursorCurr = cursorMax
 		} else if (removing) {
 			for (var i = cursorCurr; i >= 0; i--) {
-				cursorCurr = i
+				cursorCurr = i;
 				if (rexps[i] && !rexps[i + 1]) {
 					break
 				}
@@ -101,28 +106,27 @@ var MaskedInput = React.createClass({
 			}
 		} else {
 			for (var i = cursorCurr; i <= cursorMax; i++) {
-				cursorCurr = i
+				cursorCurr = i;
 				if (!rexps[i + 1] && rexps[i]) {
 					break
 				}
 				if (rexps[i + 1] && rexps[i + 1].test(newValue[i])) {
 					if (!rexps[i]) {
-						cursorCurr++
+						cursorCurr++;
 					}
 					break
 				}
 			}
 		}
-
-		this.mask.empty = cursorMax == cursorMin
-		this.mask.props.value = newValue
-		this.mask.cursor = cursorCurr
+		this.mask.empty = cursorMax == cursorMin;
+		this.mask.props.value = newValue;
+		this.mask.cursor = cursorCurr;
 	},
 
 	_onBlur: function (e) {
 		if (this.props.mask) {
-			var cursor = this.mask.cursor
-			var value = this.mask.props.value
+			var cursor = this.mask.cursor;
+			var value = this.mask.props.value;
 
 			if (!this.mask.empty) {
 				//this.mask.props.value = value.substr(0, cursor) //wtf?
@@ -139,7 +143,7 @@ var MaskedInput = React.createClass({
 
 	_onChange: function (e) {
 		if (this.props.mask) {
-			this.processValue(e.target.value)
+			this.processValue(e.target.value);
 			this.forceUpdate()
 		}
 		if (this.props.onChange) {
