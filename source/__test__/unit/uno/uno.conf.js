ffbb03f8aeeaf39decc179be72b494f11835cd4e
Wert_Lex <a.v.povar@gmail.com>
2015-09-30 00:39:34 +0300

    Not so straight Karma launcher

---
+++ source/__test__/unit/uno/uno.conf.js
@@ -0,0 +1,3 @@
+/**
+ * Created by wert on 29.09.15.
+ */
---
+++ source/__test__/unit/uno/uno.spec.js
@@ -0,0 +1,8 @@
+describe('uno', function () {
+    it('should say "uno"', function () {
+        expect(uno()).to.equal('uno');
+        //expect('World').to.equal('World');
+    });
+});
+
+
