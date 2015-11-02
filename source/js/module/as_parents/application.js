e971cbda1f108fea65c0118b38c403ca003daced
brightboahen <bridark17@gmail.com>
2015-11-02 14:31:17 +0000

    caching issue hotfix added functionality to log out users and clear cookie

--- source/js/module/as_parents/application.js
+++ source/js/module/as_parents/application.js
@@ -11,10 +11,12 @@ ApplicationView = React.createClass({
         document.addEventListener(appVisibility.visibilityChange,function(){
                 var obj = Helpers.pageVisibility.checkVisibilityOptions();
                 if(document[obj.hidden]==true){
-                    Helpers.cookie.remove('authorizationInfo');
-                    binding.sub('authorizationInfo').clear();
-                    document.location.hash = '#login';
-                    document.location.reload(true);
+                    if(Helpers.cookie.get('authorizationInfo') !== undefined){
+                        Helpers.cookie.remove('authorizationInfo');
+                        binding.sub('authorizationInfo').clear();
+                        document.location.hash = '#login';
+                        document.location.reload(true);
+                    }
                 }
             }
         );
