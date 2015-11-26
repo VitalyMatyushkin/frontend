33f31e0ece129ccfa424b21689b1fb692c07b4dc
nurbek-ab <nurbek.ab@gmail.com>
2015-11-26 12:04:49 +0600

    Bug #343 - When you minimize the browser window flips to the login page

--- source/js/module/as_admin/application.js
+++ source/js/module/as_admin/application.js
@@ -7,23 +7,6 @@ var HeadView = require('module/as_admin/head'),
 
 ApplicationView = React.createClass({
     mixins: [Morearty.Mixin],
-    componentDidMount:function(){
-        var self = this,
-            binding = self.getDefaultBinding(),
-            appVisibility = Helpers.pageVisibility.checkVisibilityOptions();
-        document.addEventListener(appVisibility.visibilityChange,function(){
-                var obj = Helpers.pageVisibility.checkVisibilityOptions();
-                if(document[obj.hidden]==true){
-                    if(Helpers.cookie.get('authorizationInfo') !== undefined){
-                        Helpers.cookie.remove('authorizationInfo');
-                        binding.sub('authorizationInfo').clear();
-                        document.location.hash = '#login';
-                        document.location.reload(true);
-                    }
-                }
-            }
-        );
-    },
     render: function() {
         var self = this,
             binding = self.getDefaultBinding();
