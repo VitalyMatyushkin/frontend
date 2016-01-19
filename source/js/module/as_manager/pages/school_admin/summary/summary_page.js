f5f5359e733695ddb97e5c35c8568f9fb0e334dd
heavevetal <heavyvetal88@mail.ru>
2016-01-19 17:34:20 +0600

    Edit summary_page.js

--- source/js/module/as_manager/pages/school_admin/summary/summary_page.js
+++ source/js/module/as_manager/pages/school_admin/summary/summary_page.js
@@ -1,68 +1,80 @@
-const   SVG         = require('module/ui/svg'),
-        Map         = require('module/ui/map/map'),
-        React       = require('react'),
-        ReactDOM    = require('reactDom'),
-        If          = require('module/ui/if/if'),
-        Immutable   = require('immutable');
+const SVG = require('module/ui/svg'),
+    Map = require('module/ui/map/map'),
+    React = require('react'),
+    ReactDOM = require('reactDom'),
+    If = require('module/ui/if/if'),
+    Immutable = require('immutable');
 
 const SchoolSummary = React.createClass({
-    mixins: [Morearty.Mixin],
-    componentWillMount: function() {
-        var self = this,
-            binding = self.getDefaultBinding(),
-            globalBinding = self.getMoreartyContext().getBinding(),
-            activeSchoolId = globalBinding.get('userRules.activeSchoolId');
+  mixins: [Morearty.Mixin],
+  componentWillMount: function () {
+    var self = this,
+        binding = self.getDefaultBinding(),
+        globalBinding = self.getMoreartyContext().getBinding(),
+        activeSchoolId = globalBinding.get('userRules.activeSchoolId');
 
-        self.activeSchoolId = activeSchoolId;
+    self.activeSchoolId = activeSchoolId;
 
-		window.Server.school.get(
-			{
-				id: activeSchoolId,
-				filter: {include: 'postcode'}
-			}
-        ).then(function(data) {
-            binding.set(Immutable.fromJS(data));
-            self.isMounted() && self.forceUpdate();
+    window.Server.school.get(
+        {
+          id: activeSchoolId,
+          filter: {include: 'postcode'}
+        }
+    ).then(function (data) {
+          binding.set(Immutable.fromJS(data));
+          self.isMounted() && self.forceUpdate();
         });
-    },
-    componentWillUnmount: function() {
-        var self = this;
+  },
+  componentWillUnmount: function () {
+    var self = this;
 
-        self.request && self.request.cancel();
-    },
-    render: function() {
-        var self = this,
-            binding = self.getDefaultBinding(),
-            schoolPicture = binding.get('pic'),
-            siteLink = binding.get('domain') + '.stage.squadintouch.com',
-            geoPoint = binding.toJS('postcode.point');
+    self.request && self.request.cancel();
+  },
+  render: function () {
+    var self = this,
+        binding = self.getDefaultBinding(),
+        schoolPicture = binding.get('pic'),
+        siteLink = binding.get('domain') + '.stage.squadintouch.com',
+        geoPoint = binding.toJS('postcode.point');
 
-        return (
-            <div>
-                <h1 className="eSchoolMaster_title">
-                    {schoolPicture ? <div className="eSchoolMaster_flag"><img src={schoolPicture}/></div> : ''}
-                    {binding.get('name')}
+    return (
+        <div>
+          <div className="changeSchool">
+            <a title="Change active school" href="/#schools" className="changeButton"><img
+                src="images/change_school.png"></img></a>
+          </div>
+          <div className="eSchoolMaster_summary">
+            <div className="summary_inside">
+              <div className="editSchool">
+                <a href={'/#schools/edit?id=' + self.activeSchoolId}>
+                  <img src="images/edit_school.png"></img>
+                </a>
+              </div>
+              <h1 className="eSchoolMaster_title">
+                {schoolPicture ? <div className="eSchoolMaster_flag"><img src={schoolPicture}/></div> : ''}
+                {binding.get('name')}
 
-                    <div className="eSchoolMaster_buttons">
+              </h1>
 
-                        <a href={'/#schools/edit?id=' + self.activeSchoolId} className="bButton">Edit...</a>
-                        <a href="/#schools" className="bButton">Change active school...</a>
-                    </div>
-                </h1>
-                <p>PostCode: {binding.get('postcodeId')}</p>
+              <p>PostCode: {binding.get('postcodeId')}</p>
 
-                <p>Address: {binding.get('address')}</p>
+              <p>Address: {binding.get('address')}</p>
 
+              <div className="eDescription">
                 <p>Description: {binding.get('description')}</p>
-
-                <p>Site: <a href={'//' + siteLink} target="blank" title="binding.get('name') homepage">http://{siteLink}</a></p>
-
-                <If condition={geoPoint !== undefined}>
-                    <Map binding={binding} point={binding.toJS('postcode.point')}/>
-                </If>
+              </div>
+              <p>Site: <a href={'//' + siteLink} target="blank"
+                          title="binding.get('name') homepage">http://{siteLink}</a>
+              </p>
             </div>
-        )
-    }
+            <If condition={geoPoint !== undefined}>
+              <Map binding={binding} point={binding.toJS('postcode.point')}/>
+            </If>
+
+          </div>
+        </div>
+    )
+  }
 });
 
 
