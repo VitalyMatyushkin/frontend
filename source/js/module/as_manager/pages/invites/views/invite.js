fc24596dae3e830ee21b45ba270189e7e41dceb8
heavevetal <heavyvetal88@mail.ru>
2016-01-25 17:55:21 +0600

    Makup outbox/inbox

--- source/js/module/as_manager/pages/invites/views/invite.js
+++ source/js/module/as_manager/pages/invites/views/invite.js
@@ -42,30 +42,33 @@ InviteView = React.createClass({
 			isArchive = typeof binding.get('accepted') === 'boolean',
             inviter = self.getBinding('inviter'),
             invited = self.getBinding('invited'),
+            schoolPicture = binding.get('pic'),
             sport = binding.get('event.sport.name'),
             message = binding.get('message') || '',
             isRedeemed = binding.get('redeemed'),
             startDate = (new Date(binding.get('event.startTime'))).toLocaleString();
 
         return <div key={binding.get('id')} className={inviteClasses}>
+            <div className="eInvite_img"><img src={schoolPicture}/></div>
+            <div className="eInviteWrap">
             <div className="eInvite_header">
                 <span className="eInvite_eventName">
-                    {binding.get('inviter').get('name')}
-                    <span className="eInvite_vs">VS</span>
-                    {binding.get('guest').get('name')}
+                    {isInbox ?  binding.get('inviter')  .get('name') : null}
+                    {isOutBox ? binding.get('guest').get('name') : null}
                 </span>
                 <span className="eInvite_eventDate"></span>
             </div>
-            <div className="eInvite_message">{isInbox || isArchive ? message : 'Awaiting opponent...' }</div>
             <div className="eInvite_info">
                 <div>{'Sport:'} {sport}</div>
                 <div>{'Start date:'} {startDate}</div>
+                <div>{'Age:'} </div>
             </div>
+            <div className="eInvite_message">{isInbox || isArchive ? message : 'Awaiting opponent...' }</div>
 			<div className="eInvite_buttons">
 				{isInbox ? <a href={'/#invites/' + binding.get('id') + '/accept'} className="bButton">Accept</a> : null}
 				{isInbox ? <a href={'/#invites/' + binding.get('id') + '/decline'} className="bButton mRed">Decline</a> : null}
 				{isOutBox ? <a href={'/#invites/' + binding.get('id') + '/cancel'} className="bButton mRed">Cancel</a> : null}
-			</div>
+			</div></div>
             {!isRedeemed && isInbox? <span className="eInvite_redeemed" onClick={self.onClickRedeemed}>{'was read?'}</span> : null}
         </div>;
     }
