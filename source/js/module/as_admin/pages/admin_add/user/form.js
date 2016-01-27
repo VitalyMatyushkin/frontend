41819de4568d02f767feabc0281e14ea895f7c30
brightboahen <bridark17@gmail.com>
2016-01-27 14:08:30 +0000

    #397 create a basic user with no afiliations

--- source/js/module/as_admin/pages/admin_add/user/form.js
+++ source/js/module/as_admin/pages/admin_add/user/form.js
@@ -1,6 +1,7 @@
 var Form = require('module/ui/form/form'),
 	FormField = require('module/ui/form/form_field'),
 	React = require('react'),
+	ReactDOM = require('reactDom'),
 	RegiseterUserForm;
 
 RegiseterUserForm = React.createClass({
@@ -11,29 +12,18 @@ RegiseterUserForm = React.createClass({
 	},
 	onSuccess: function() {
 		var self = this;
-
 		self.showForm = false;
 	},
-	_selectChanged:function(){
-		console.log('changed');
-	},
 	render: function() {
-		var self = this;
-
+		var self = this,
+			binding = self.getDefaultBinding();
 		return (
-			<Form name="Sign up" service="users" binding={self.getDefaultBinding()} onSuccess={self.props.onSuccess}>
+			<Form name="Create New User" updateBinding={true} service="users" binding={binding} onSuccess={self.props.onSuccess}>
 				<FormField type="text" field="username" validation="alphanumeric server">Username</FormField>
 				<FormField type="text" field="firstName" validation="required alphanumeric">First name</FormField>
 				<FormField type="text" field="lastName" validation="required alphanumeric">Last name</FormField>
 				<FormField type="confirmText" field="email" validation="required email server">Email</FormField>
 				<FormField type="confirmText" textType="password" field="password" validation="required">Password</FormField>
-				<label>Select role</label>
-				<select id="roleSelect" onChange={self._selectChanged}>
-					<option value="manager">Manager</option>
-					<option value="coach">Coach</option>
-					<option value="teacher">PE Teacher</option>
-					<option value="admin">Admin</option>
-				</select>
 			</Form>
 		)
 	}
