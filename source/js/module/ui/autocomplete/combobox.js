5e43a7798531b38f7b723b9beefcf6d8aa8285a4
brightboahen <bridark17@gmail.com>
2016-02-03 19:20:28 +0000

    form edit pre-population

--- source/js/module/ui/autocomplete/combobox.js
+++ source/js/module/ui/autocomplete/combobox.js
@@ -101,18 +101,7 @@ module.exports = React.createClass({
 		self.setState({
 			menu: self.makeMenu(newProps.children)
 		});
-
-		self.defValueTimerId = setTimeout(function(){
-			if(newProps && newProps.value!==null){
-				ReactDOM.findDOMNode(self.refs.input).value=self.findInputValue(newProps.value);
-			}
-		},200);
-		//==================================================//
-		//You can not directly change the DOM! Use State!!!	//
-		//==================================================//
-		//self.defValueTimerId = setTimeout(function(){
-		//	ReactDOM.findDOMNode(self.refs.input).value=self.findInputValue(newProps.value);
-		//},200);
+		self.props.binding.set('inputValue', self.findInputValue(newProps.value));
 	},
 
 	/**
