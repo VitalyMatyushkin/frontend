01ac6961fe7b685fe6bf41e0ba78d3e72710575b
brightboahen <bridark17@gmail.com>
2016-02-17 17:31:19 +0000

    Features #502, #503, #506 registration steps modification

--- source/js/module/ui/register/user/register_done.js
+++ source/js/module/ui/register/user/register_done.js
@@ -9,17 +9,23 @@ RegiseterUserDone = React.createClass({
 		onSuccess: React.PropTypes.func
 	},
 	render: function() {
-		var self = this;
-
+		var self = this,
+			binding = self.getDefaultBinding();
 		return (
 			<div className="bPageMessage">
 				<h2>Registration almost done</h2>
-
-				<p className="bRegisterFinish">
-					Expect confirmation of the invitation.
-					Now you can go to your profile page and fill additional information.
-				</p>
-
+				{/*Check the type of account registered for by the user and display information accordingly*/}
+				<If condition={(binding.get('type')==='teacher' || binding.get('type')==='coach' || binding.get('type')=='parent')}>
+					<p className="bRegisterFinish">
+						Please be aware that joining requests are confirmed by school administrators and sometimes there can be delays with receiving confirmation.
+					</p>
+				</If>
+				<If condition={(binding.get('type')==='manager' || binding.get('type')=== 'admin')}>
+					<p className="bRegisterFinish">
+						Thank you for joining SquadInTouch. Joining requests are usually confirmed within 1 working day.
+						Meanwhile you can log in to your account for managing your profile details.
+					</p>
+				</If>
 				<div className="bButton" onClick={self.props.onSuccess}>Finish</div>
 			</div>
 		)
