431950654327961542b1c7ed50a0a81dd700329d
Oleg Pikhnenko <mtbstreet91@gmail.com>
2016-03-01 06:24:56 +0600

    fix teams mode for invite page

--- source/js/module/as_manager/pages/invites/views/accept.js
+++ source/js/module/as_manager/pages/invites/views/accept.js
@@ -61,35 +61,70 @@ const InviteAcceptView = React.createClass({
         var self = this,
             binding = self.getDefaultBinding();
 
-        window.Server.participants.post({eventId: binding.get('model.id')}, {
-            eventId: binding.get('model.id'),
-            schoolId: binding.get('rivals.0.id'),
-            sportId: binding.get('model.sportId'),
-            name: binding.get('rivals.0.name')
-        }).then(function (res) {
+        if(self._isEventDataCorrect()) {
+            let mode = binding.get('mode.0');
 
-            window.Server.inviteRepay.post({inviteId: binding.get('invite.id')}, {
-                teamId: res.id,
-                accepted: true
-            });
-
-            binding.get('players.0').forEach(function (student, studentIndex) {
-                window.Server.playersRelation.put({
-                    teamId: res.id,
-                    studentId: student.get('id')
-                }).then(function () {
-                    binding.sub('players.0.' + studentIndex).meta().set('sync', true);
-
-                    var allSynced = binding.get('players.0').every(function (model, modelIndex) {
-                        return binding.sub('players.0.' + modelIndex).meta().get('sync');
+            switch (mode) {
+                case 'teams':
+                    window.Server.relParticipants.put(
+                        {
+                            eventId: binding.get('model.id'),
+                            teamId: binding.get('teamModeView.teamViewer.0.selectedTeamId')
+                        },
+                        {
+                            eventId: binding.get('model.id'),
+                            teamId: binding.get('teamModeView.teamViewer.0.selectedTeamId')
+                        }
+                    ).then(() => {
+                        return window.Server.inviteRepay.post({inviteId: binding.get('invite.id')}, {
+                            teamId: binding.get('teamModeView.teamViewer.0.selectedTeamId'),
+                            accepted: true
+                        }).then((res) => {
+                            document.location.hash = '#event/' + binding.get('model.id');
+                            return res;
+                        });
                     });
+                    break;
+                case 'temp':
+                    window.Server.participants.post({eventId: binding.get('model.id')}, {
+                        eventId: binding.get('model.id'),
+                        schoolId: binding.get('rivals.0.id'),
+                        sportId: binding.get('model.sportId'),
+                        name: binding.get('rivals.0.name')
+                    }).then(function (res) {
 
-                    if (allSynced) {
-                        document.location.hash = '#event/' + binding.get('model.id');
-                    }
-                });
-            });
-        });
+                        window.Server.inviteRepay.post({inviteId: binding.get('invite.id')}, {
+                            teamId: res.id,
+                            accepted: true
+                        });
+
+                        binding.get('players.0').forEach(function (student, studentIndex) {
+                            window.Server.playersRelation.put({
+                                teamId: res.id,
+                                studentId: student.get('id')
+                            }).then(function (res) {
+                                binding.sub('players.0.' + studentIndex).meta().set('sync', true);
+
+                                var allSynced = binding.get('players.0').every(function (model, modelIndex) {
+                                    return binding.sub('players.0.' + modelIndex).meta().get('sync');
+                                });
+
+                                if (allSynced) {
+                                    document.location.hash = '#event/' + binding.get('model.id');
+                                }
+                                return res;
+                            });
+                        });
+                        return res;
+                    });
+                    break;
+            };
+        }
+    },
+    _isEventDataCorrect: function() {
+        const self = this;
+
+        return !self.getDefaultBinding().toJS('error.0').isError;
     },
     render: function() {
         var self = this,
