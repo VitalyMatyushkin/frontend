edbf5d5f9877f465fd2ba6089308c12e2b100d4d
Anatoly <atolmak@gmail.com>
2016-06-06 17:44:07 +0600

    Bug #818 Role is not removed

--- source/js/module/shared_pages/users/user_roles.js
+++ source/js/module/shared_pages/users/user_roles.js
@@ -1,54 +1,49 @@
 /**
  * Created by bridark on 30/06/15.
  */
-var UserRole,
-    SVG = require('module/ui/svg'),
-    React = require('react'),
-    userRoles;
+const 	SVG 	= require('module/ui/svg'),
+    	React 	= require('react');
 
-UserRole = React.createClass({
+const UserRole = React.createClass({
     mixins:[Morearty.Mixin],
+	revokeRole: function (permissionId) {
+		const 	self 	= this,
+				binding = self.getDefaultBinding(),
+				userId 	= binding.toJS('userWithPermissionDetail.id'),
+				cf 		= confirm("Are you sure you want to revoke this permission?");
+
+		if(cf === true){
+			window.Server.userPermission.delete({userId:userId, permissionId:permissionId})
+				.then(function(res){
+					console.log(res);
+					alert('Role successfully revoked');
+				});
+		}
+	},
     getRoleData:function(){
-        var self, binding;
-        self = this;
-        var revoke = function (roleId) {
-            return function(event){
-                //var cf = confirm("Are you sure you want to revoke this permission?");
-                //if(cf === true){
-                //    window.Server.userPermission.delete(roleId)
-                //        .then(function(res){
-                //            console.log(res);
-                //            alert('Role successfully revoked');
-                //        });
-                //}
-                alert('not implemented!');
-                event.stopPropagation();
-            }
-        };
-        binding = self.getDefaultBinding();
-        userRoles = binding.get('selectedUser').role;
+        const 	self 		= this,
+				binding 	= self.getDefaultBinding(),
+				permissions = binding.toJS('userWithPermissionDetail.permissions');
+
         var tempArray;
-        if(typeof userRoles !== 'undefined'){
-            tempArray = userRoles.map(function(role){
+        if(typeof permissions !== 'undefined'){
+            tempArray = permissions.map(function(permission){
                 return(
-                    <div key={role.id} className="bPopupEdit_row bRole">
-                        <div className="bPopupEdit_role" style={{width:240+'px'}}>{role.school ? role.school.name:''}</div>
-                        <div className="bPopupEdit_role" style={{width:180+'px'}}>{role.preset}</div>
-                        <div className="bPopupEdit_role">{typeof role.student !== 'undefined'?role.student.firstName+" "+role.student.lastName:''}</div>
-                        <span><SVG classes="bIcon-mod" icon="icon_blocked"/></span>
-                        <span onClick={revoke(role.id)}><SVG classes="bIcon-mod" icon="icon_trash" /></span>
+                    <div key={permission.id || permission._id} className="bPopupEdit_row bRole">
+                        <div className="bPopupEdit_role" style={{width:240+'px'}}>{permission.school ? permission.school.name:''}</div>
+						<div className="bPopupEdit_role" style={{width:180+'px'}}>{permission.preset}</div>
+						<div className="bPopupEdit_role" style={{width:180+'px'}}>{permission.comment}</div>
+                        <span onClick={self.revokeRole.bind(null, permission.id || permission._id)}><SVG classes="bIcon-mod" icon="icon_trash" /></span>
                     </div>
                 )
             });
-            return userRoles = tempArray;
+            return tempArray;
         }
     },
     render:function(){
-        var self  = this,
-            binding = self.getDefaultBinding();
-        if(typeof binding.get('selectedUser').role !== 'undefined'){
-            if(binding.get('popup') === true) self.getRoleData();
-        }
+        const 	self  	= this,
+            	binding = self.getDefaultBinding();
+
         return <div>
                     <div className="bPopupEdit_row" style={{marginTop:20+'px', borderTop: 2+'px solid black',borderBottom: 2+'px solid black'}}>
                         <div className="bPopupEdit_roleHead" style={{width:240+'px'}}>School</div>
@@ -56,8 +51,10 @@ UserRole = React.createClass({
                         <div className="bPopupEdit_roleHead">Details</div>
                         <div className="bPopupEdit_roleHead" style={{width:30+'px'}}>Actions</div>
                     </div>
-                {userRoles}
+                {self.getRoleData()}
             </div>;
     }
 });
+
+
 module.exports = UserRole;
