e6fd7c6166102fa4f096adaf0ece8c63dc714a94
Anatoly <atolmak@gmail.com>
2016-06-14 18:18:00 +0600

    Feature #933 Add checkbox form field

--- source/js/module/shared_pages/settings/general/general_page.js
+++ source/js/module/shared_pages/settings/general/general_page.js
@@ -38,7 +38,7 @@ const GeneralSettingsPage = React.createClass({
 						<FormField labelText="+" type="imageFile" typeOfFile="image" field="avatar"/>
 						<FormField type="text" field="firstName" validation="required alphanumeric">Name</FormField>
 						<FormField type="text" field="lastName" validation="required alphanumeric">Surname</FormField>
-						<FormField type="date" field="birthday">Birthday</FormField>
+						<FormField type="date" field="birthday" validation="required">Birthday</FormField>
 					</FormColumn>
 					<FormColumn type="column">
 						<h3>VERIFICATION INFORMATION</h3>
