5b3f6eb389702fe65da37b9e0133ae804f1c8af8
Anatoly <atolmak@gmail.com>
2016-06-21 15:59:07 +0600

    Bug #988 Private in album don't work

--- source/js/module/ui/gallery/album/album_edit_form.js
+++ source/js/module/ui/gallery/album/album_edit_form.js
@@ -22,15 +22,6 @@ const AlbumEditForm = React.createClass({
 			<Form name={self.props.title} onSubmit={self.props.onFormSubmit} binding={binding} >
 				<FormColumn>
 					<FormField type="text" field="name" validation="required">Name: </FormField>
-					<span>Access mode:</span>
-					<select
-						style={{margin: '5px 0px'}}
-						defaultValue='everyone'
-						value={binding.get('accessMode')}
-						onChange={self.changeAccessMode}>
-						<Morearty.DOM.option key="everyone-type" value='everyone'>everyone</Morearty.DOM.option>
-						<Morearty.DOM.option key="private-type" value='private'>private</Morearty.DOM.option>
-					</select>
 				</FormColumn>
 			</Form>
 		);
