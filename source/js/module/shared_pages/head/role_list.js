dc989bdae75bddf6d7dc5e56a59edd857c261847
Anatoly <atolmak@gmail.com>
2016-06-24 20:23:20 +0600

    Bug #1010 The screen of locked user

--- source/js/module/shared_pages/head/role_list.js
+++ source/js/module/shared_pages/head/role_list.js
@@ -163,7 +163,7 @@ const  RoleList = React.createClass({
 				binding 	= self.getDefaultBinding(),
 				role        = self.getMoreartyContext().getBinding().get('userData.authorizationInfo.role'),
 				listOpen    = binding.get('listOpen'),
-				show        = !!binding.get('permissions').length && role,
+				show        = !!binding.get('permissions').length && !!role,
 				hidden      = !binding.get('schools').length && !role && !self.props.onlyLogout;
 
 		if(listOpen)
