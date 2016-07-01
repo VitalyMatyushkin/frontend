45bb08059ec900d3df6df52a1668a9f3836aa25d
Anatoly <atolmak@gmail.com>
2016-07-01 10:09:16 +0600

    Bug #905 School form layout issue

--- source/js/module/as_manager/pages/schools/schools_form.js
+++ source/js/module/as_manager/pages/schools/schools_form.js
@@ -21,25 +21,25 @@ const SchoolForm = React.createClass({
 		return (
 			<Form name={self.props.title} binding={self.getDefaultBinding()} onSubmit={self.props.onSubmit}>
 				<FormColumn>
-					<FormField labelText="+" type="imageFile" typeOfFile="image" field="pic"/>
+					<FormField type="imageFile" field="pic" labelText="+" typeOfFile="image"/>
 
-					<FormField type="text" field="name" validation="required">Name</FormField>
-					<FormField type="text" field="description" validation="required">Description</FormField>
-					<FormField type="phone" field="phone" validation="required">Phone</FormField>
+					<FormField type="text" field="email" validation="required email" fieldClassName="mLarge">
+						School Official Email
+					</FormField>
+					<FormField type="text" field="sportsDepartmentEmail" validation="required email" fieldClassName="mLarge">
+						Sports Department Email
+					</FormField>
 				</FormColumn>
 				<FormColumn>
+					<FormField type="text" field="name" validation="required">Name</FormField>
+					<FormField type="textarea" field="description" validation="required">Description</FormField>
+					<FormField type="phone" field="phone" validation="required">Phone</FormField>
 					<FormField type="area" field="postcodeId" defaultItem={postcode}
 							   validation="any">Postcode</FormField>
 					<FormField type="text" field="address" validation="required">Address</FormField>
 					<FormField type="text" field="domain" validation="required">Domain</FormField>
+					<FormField type="dropdown" field="status">School Status</FormField>
 				</FormColumn>
-                <FormColumn type="column">
-                    <FormField type="text" field="email" validation="required email" fieldClassName="mLarge">School Official Email</FormField>
-                    <FormField type="text" field="sportsDepartmentEmail" validation="required email" fieldClassName="mLarge">Sports Department Email</FormField>
-                </FormColumn>
-                <FormColumn type="column">
-                    <FormField type="dropdown" field="status">School Status</FormField>
-                </FormColumn>
 			</Form>
 		)
 	}
