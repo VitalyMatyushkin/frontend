955da31b2c5660ab8b4161084dcf1db00d02135c
heavevetal <heavyvetal88@mail.ru>
2016-07-08 10:58:32 +0600

    Bug #1091, Bug #1088

--- source/js/module/ui/autocomplete2/ComboBox2.js
+++ source/js/module/ui/autocomplete2/ComboBox2.js
@@ -344,7 +344,7 @@ const ComboBox2 = React.createClass({
         // this will act instead of loader spinner for a while
         const loaderStyle = {
             position:    'absolute',
-            top:         '10px',
+            top:         '3px',
             right:       '3px',
             height:      '17px',
             display:      self.state.isLoading ? undefined : "none"
