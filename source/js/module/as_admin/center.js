3d83bfd944b6d2f52c5f7d9e37da74dc39d19394
Alex V. Povar <a.v.povar@gmail.com>
2016-07-28 18:37:50 +0600

    Admin's center is refactored

--- source/js/module/as_admin/center.js
+++ source/js/module/as_admin/center.js
@@ -1,14 +1,23 @@
 /**
  * Created by bridark on 04/06/15.
  */
-const   RouterView      = require('module/core/router'),
-        Route           = require('module/core/route'),
-        LoginRoute      = require('module/core/routes/login_route'),
-        LogoutRoute     = require('module/core/routes/logout_route'),
-        RegisterRoute   = require('module/core/routes/register_route'),
-        SettingsRoute   = require('module/core/routes/settings_route'),
-        Morearty        = require('morearty'),
-        React           = require('react');
+const   RouterView      		= require('module/core/router'),
+        Route           		= require('module/core/route'),
+        LoginRoute      		= require('module/core/routes/login_route'),
+        LogoutRoute     		= require('module/core/routes/logout_route'),
+        RegisterRoute   		= require('module/core/routes/register_route'),
+        SettingsRoute   		= require('module/core/routes/settings_route'),
+        Morearty        		= require('morearty'),
+        React           		= require('react'),
+        SchoolPageComponent 	= require('module/as_school/pages/school/school_page'),
+		FixturePageComponent 	= require("module/as_school/pages/fixtures/fixtures_page"),
+		EventPageComponent 		= require('module/as_school/pages/event/event_page'),
+		CalendarPageComponent 	= require('module/as_school/pages/calendar/calendar_page'),
+		OpponentsPageComponent 	= require('module/as_school/pages/opponents/opponents_page'),
+		SchoolAddComponent		= require('module/as_manager/pages/schools/schools_add'),
+		AlbumsComponent			= require('module/ui/gallery/albums'),
+		AdminDashboardComponent	= require('module/as_admin/pages/admin_schools/admin_dashboard'),
+		SchoolSandboxComponent	= require('module/as_admin/pages/admin_schools/admin_dashboard');
 
 const Center = React.createClass({
     mixins: [Morearty.Mixin],
@@ -16,30 +25,30 @@ const Center = React.createClass({
         return Morearty.MergeStrategy.MERGE_REPLACE;
     },
     render: function() {
-        var self = this,
-            binding = self.getDefaultBinding(),
-            currentPage =  binding.get('routing.currentPageName') || '',
-            mainClass = 'bMainLayout mClearFix m' + currentPage.charAt(0).toUpperCase() + currentPage.slice(1);
+        const 	self 		= this,
+            	binding 	= self.getDefaultBinding(),
+            	currentPage =  binding.get('routing.currentPageName') || '',
+            	mainClass 	= 'bMainLayout mClearFix m' + currentPage.charAt(0).toUpperCase() + currentPage.slice(1);
 
         return (
             <div className={mainClass}>
                 <div className="bPageWrap">
                     <RouterView routes={ binding.sub('routing') } binding={binding}>
-                        <Route path="/ /school /school/:subPage" binding={binding.sub('schoolProfile')} component="module/as_school/pages/school/school_page"  />
-                        <Route path="/fixtures" binding={binding.sub('schoolFixtures')} component="module/as_school/pages/fixtures/fixtures_page"  />
-                        <Route path="/event" binding={binding.sub('schoolEvent')} component="module/as_school/pages/event/event_page"  />
-                        <Route path="/calendar" binding={binding.sub('schoolCalendar')} component="module/as_school/pages/calendar/calendar_page"  />
-                        <Route path="/opponents/:subPage" binding={binding.sub('opponentsList')} component="module/as_school/pages/opponents/opponents_page"  />
-                        <Route path="/schools/add " binding={binding.sub('schoolsPage')} component="module/as_manager/pages/schools/schools_add"	/>
-                        <Route path="/albums /albums/:albumId" binding={binding.sub('albums')} component="module/ui/gallery/albums"  />
+                        <Route path="/ /school /school/:subPage"    binding={binding.sub('schoolProfile')}      component={SchoolPageComponent}/>
+                        <Route path="/fixtures"                     binding={binding.sub('schoolFixtures')}     component={FixturePageComponent}/>
+                        <Route path="/event"                        binding={binding.sub('schoolEvent')}        component={EventPageComponent}/>
+                        <Route path="/calendar"                     binding={binding.sub('schoolCalendar')}     component={CalendarPageComponent}/>
+                        <Route path="/opponents/:subPage"           binding={binding.sub('opponentsList')}      component={OpponentsPageComponent}/>
+                        <Route path="/schools/add "                 binding={binding.sub('schoolsPage')}        component={SchoolAddComponent}/>
+                        <Route path="/albums /albums/:albumId"      binding={binding.sub('albums')}             component={AlbumsComponent} />
                         <Route path="/admin_schools /admin_schools/:subPage /admin_schools/:subFolder/:subPage /admin_schools/:subFolder/:subPage/:mode /user/view"
-                               binding={binding.sub('adminSchoolList')} component="module/as_admin/pages/admin_schools/admin_dashboard"/>
+                                                                    binding={binding.sub('adminSchoolList')}    component={AdminDashboardComponent}/>
                         <Route path="/school_sandbox/:schoolId school_sandbox/:schoolId/:subPage /school_sandbox/:schoolId/:subPage/:mode /school_sandbox/:schoolId/:subPage/:mode/:id"
-                               binding={binding.sub('adminSchoolList')} component="module/as_admin/pages/admin_schools/school_sandbox/sandbox"/>
-                        <LoginRoute binding={binding.sub('userData')}  />
-                        <LogoutRoute binding={binding.sub('userData')}  />
-                        <SettingsRoute binding={binding.sub('userData')} />
-                        <RegisterRoute binding={binding.sub('form.register')}  />
+                               binding={binding.sub('adminSchoolList')} component={SchoolSandboxComponent}/>
+                        <LoginRoute                                 binding={binding.sub('userData')}/>
+                        <LogoutRoute                                binding={binding.sub('userData')}/>
+                        <SettingsRoute                              binding={binding.sub('userData')}/>
+                        <RegisterRoute                              binding={binding.sub('form.register')}/>
                     </RouterView>
                 </div>
             </div>
