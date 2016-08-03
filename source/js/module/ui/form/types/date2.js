6f69821ceac8f702cc4b08f7b7727de19ff63b1b
Anatoly <atolmak@gmail.com>
2016-08-03 20:21:10 +0600

    Feature #1199 BetweenDate component

--- source/js/module/ui/form/types/date2.js
+++ source/js/module/ui/form/types/date2.js
@@ -1,11 +1,14 @@
-const 	TypeMixin 	= require('module/ui/form/types/type_mixin'),
-		MaskedInput = require('module/ui/masked_input'),
-		React 		= require('react'),
-		Morearty	= require('morearty'),
-    	Immutable 	= require('immutable');
+const 	MaskedInput = require('module/ui/masked_input'),
+		DateHelper 	= require('module/helpers/date_helper'),
+		React 		= require('react');
 
 const TypeDate =  React.createClass({
-	mixins: [Morearty.Mixin, TypeMixin],
+	propTypes: {
+		value:			React.PropTypes.string,
+		onChange: 		React.PropTypes.func,
+		locales: 		React.PropTypes.string,
+		options:		React.PropTypes.object
+	},
     getDefaultProps: function() {
         return {
             locales:'en-GB',
@@ -16,37 +19,20 @@ const TypeDate =  React.createClass({
             }
         };
     },
-    getDefaultState: function() {
-        return Immutable.fromJS({
-            localValue:'',  //local format date
-            value:'',       //ISO format date
-            defaultValue:'' //initial date
-        });
-    },
 	componentWillMount: function() {
-		const self 			= this,
-			binding 		= self.getDefaultBinding();
+		const self 			= this;
 
 		self.setLocalValue();
-        self.addBindingListener(binding, 'defaultValue', self.setLocalValue);
-        self.addBindingListener(binding, 'localValue', changes => self.setValue(self._toIso(changes.getCurrentValue())));
 	},
 	setLocalValue: function() {
 		const self 			= this,
-            binding 		= self.getDefaultBinding(),
-			value			= binding.get('defaultValue'),
+			value			= self.props.value,
             locales 		= self.props.locales,
             options 		= self.props.options,
 			isValid			= !!value && !self.fullValidate(value),
-			localeDate 		= isValid ? new Date(value).toLocaleDateString(locales, options).replace(/[/]/g, '.'):'',
-			isoValue 	 	= isValid ? new Date(value).toISOString().substr(0, 10) : value;
+			localeDate 		= isValid ? new Date(value).toLocaleDateString(locales, options).replace(/[/]/g, '.'):'';
 
 		if(isValid){
-			binding.atomically()
-				.set('defaultValue', isoValue)
-				.set('value', isoValue)
-				.set('localValue', localeDate)
-				.commit();
 		}
 	},
 	_toIso: function(dotString) {
@@ -67,16 +53,12 @@ const TypeDate =  React.createClass({
 	},
 	handleChange: function(e) {
 		const self = this,
-            binding = self.getDefaultBinding(),
 			inputValue = e.target.value;
 
-        binding.set('localValue', inputValue);
         e.stopPropagation();
 	},
 	render: function () {
-        const self = this,
-            binding = self.getDefaultBinding(),
-            localValue = binding.get('localValue');
+        const self = this;
 
 		return (
             <MaskedInput title="Format date dd.mm.yyyy" value={localValue} className="eDateInput"
