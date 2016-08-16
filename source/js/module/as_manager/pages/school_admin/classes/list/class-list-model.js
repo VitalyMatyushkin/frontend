2afea9ba3f74cbb2262b98efa69458416029a94a
Anatoly <atolmak@gmail.com>
2016-08-16 20:11:21 +0600

    Bug #1250 Coach shouldn't be able to edit forms

---
+++ source/js/module/as_manager/pages/school_admin/classes/list/class-list-model.js
@@ -0,0 +1,111 @@
+/**
+ * Created by Anatoly on 16.08.2016.
+ */
+
+const 	React 			= require('react'),
+		Morearty		= require('morearty'),
+		SVG 			= require('module/ui/svg'),
+		DataLoader 		= require('module/ui/grid/data-loader'),
+		GridModel 		= require('module/ui/grid/grid-model');
+
+/**
+ * ClassListModel
+ *
+ * @param {object} page
+ *
+ * */
+const ClassListModel = function(page){
+	this.getDefaultBinding = page.getDefaultBinding;
+	this.getMoreartyContext = page.getMoreartyContext;
+	this.props = page.props;
+	this.state = page.state;
+
+	this.rootBinding = this.getMoreartyContext().getBinding();
+	this.activeSchoolId = this.rootBinding.get('userRules.activeSchoolId');
+
+	this.grid = this.getGrid();
+	this.dataLoader = 	new DataLoader({
+		serviceName:'schoolForms',
+		params:		{schoolId:this.activeSchoolId},
+		grid:		this.grid,
+		onLoad: 	this.getDataLoadedHandle()
+	});
+};
+
+ClassListModel.prototype = {
+	reloadData:function(){
+		this.dataLoader.loadData();
+	},
+	onEdit: function(data) {
+		document.location.hash += '/edit?id=' + data.id;
+	},
+	getGrid: function(){
+		const 	role 			= this.rootBinding.get('userData.authorizationInfo.role'),
+				changeAllowed 	= role === "ADMIN" || role === "MANAGER";
+
+		const columns = [
+			{
+				text:'Name',
+				isSorted:true,
+				cell:{
+					dataField:'name'
+				},
+				filter:{
+					type:'string'
+				}
+			},
+			{
+				text:'Age group',
+				isSorted:true,
+				cell:{
+					dataField:'age',
+					type:'custom',
+					typeOptions:{
+						parseFunction: function(item) {return 'Y' + item.age;}
+					}
+				}
+			},
+			{
+				text:'Actions',
+				cell:{
+					type:'action-buttons',
+					typeOptions:{
+						/**
+						 * Only school admin and manager can edit or delete students.
+						 * All other users should not see that button.
+						 * */
+						onItemEdit:		changeAllowed ? this.onEdit.bind(this) : null
+					}
+				}
+			}
+		];
+
+		return new GridModel({
+			actionPanel:{
+				title:'Students',
+				showStrip:true,
+
+				/**Only school admin and manager can add new students. All other users should not see that button.*/
+				btnAdd:changeAllowed ?
+				(
+					<div className="addButton" onClick={function(){document.location.hash += '/add';}}>
+						<SVG icon="icon_add_form" />
+					</div>
+				) : null
+			},
+			columns:columns,
+			filters:{limit:30}
+		});
+	},
+	getDataLoadedHandle: function(data){
+		const self = this,
+			binding = self.getDefaultBinding();
+
+		return function(data){
+			binding.set('data', self.grid.table.data);
+		};
+	}
+};
+
+
+module.exports = ClassListModel;
