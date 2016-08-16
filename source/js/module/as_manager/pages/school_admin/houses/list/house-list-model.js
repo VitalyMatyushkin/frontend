b9177dad82164b3f7761be14d5e0569faa44061e
Anatoly <atolmak@gmail.com>
2016-08-16 20:38:46 +0600

    Bug #1249 Coach shouldn't be able to edit houses

--- source/js/module/as_manager/pages/school_admin/houses/list/house-list-model.js
+++ source/js/module/as_manager/pages/school_admin/houses/list/house-list-model.js
@@ -4,17 +4,16 @@
 
 const 	React 			= require('react'),
 		Morearty		= require('morearty'),
-		SVG 			= require('module/ui/svg'),
 		DataLoader 		= require('module/ui/grid/data-loader'),
 		GridModel 		= require('module/ui/grid/grid-model');
 
 /**
- * ClassListModel
+ * HouseListModel
  *
  * @param {object} page
  *
  * */
-const ClassListModel = function(page){
+const HouseListModel = function(page){
 	this.getDefaultBinding = page.getDefaultBinding;
 	this.getMoreartyContext = page.getMoreartyContext;
 	this.props = page.props;
@@ -25,19 +24,36 @@ const ClassListModel = function(page){
 
 	this.grid = this.getGrid();
 	this.dataLoader = 	new DataLoader({
-		serviceName:'schoolForms',
+		serviceName:'schoolHouses',
 		params:		{schoolId:this.activeSchoolId},
 		grid:		this.grid,
 		onLoad: 	this.getDataLoadedHandle()
 	});
 };
 
-ClassListModel.prototype = {
+HouseListModel.prototype = {
 	reloadData:function(){
 		this.dataLoader.loadData();
 	},
 	onEdit: function(data) {
 		document.location.hash += '/edit?id=' + data.id;
+	},
+	onRemove:function(data){
+		const 	self 		= this,
+			rootBinding = self.getMoreartyContext().getBinding(),
+			schoolId 	= rootBinding.get('userRules.activeSchoolId'),
+			binding = self.getDefaultBinding();
+
+		if(data !== undefined){
+			window.Server.schoolHouse.delete({schoolId:schoolId, houseId:data.id}).then(function(res){
+				binding.update('data',function(houses){
+					return houses.filter(function(house){
+						return house.get('id') !== data.id;
+					});
+				});
+				return res;
+			});
+		}
 	},
 	getGrid: function(){
 		const 	role 			= this.rootBinding.get('userData.authorizationInfo.role'),
@@ -45,7 +61,7 @@ ClassListModel.prototype = {
 
 		const columns = [
 			{
-				text:'Name',
+				text:'House name',
 				isSorted:true,
 				cell:{
 					dataField:'name'
@@ -55,14 +71,20 @@ ClassListModel.prototype = {
 				}
 			},
 			{
-				text:'Age group',
+				text:'Description',
 				isSorted:true,
 				cell:{
-					dataField:'age',
-					type:'custom',
-					typeOptions:{
-						parseFunction: function(item) {return 'Y' + item.age;}
-					}
+					dataField:'description'
+				},
+				filter:{
+					type:'string'
+				}
+			},
+			{
+				text:'Color',
+				cell:{
+					dataField:'colors',
+					type:'colors'
 				}
 			},
 			{
@@ -71,10 +93,11 @@ ClassListModel.prototype = {
 					type:'action-buttons',
 					typeOptions:{
 						/**
-						 * Only school admin and manager can edit or delete students.
+						 * Only school admin and manager can edit or delete houses.
 						 * All other users should not see that button.
 						 * */
-						onItemEdit:		changeAllowed ? this.onEdit.bind(this) : null
+						onItemEdit:		changeAllowed ? this.onEdit.bind(this) : null,
+						onItemRemove:	changeAllowed ? this.onRemove.bind(this) : null
 					}
 				}
 			}
@@ -82,19 +105,17 @@ ClassListModel.prototype = {
 
 		return new GridModel({
 			actionPanel:{
-				title:'Forms',
+				title:'Houses',
 				showStrip:true,
 
 				/**Only school admin and manager can add new students. All other users should not see that button.*/
 				btnAdd:changeAllowed ?
 				(
-					<div className="addButton" onClick={function(){document.location.hash += '/add';}}>
-						<SVG icon="icon_add_form" />
+					<div className="addButton addHouse" onClick={function(){document.location.hash += '/add';}}>
 					</div>
 				) : null
 			},
-			columns:columns,
-			filters:{limit:30}
+			columns:columns
 		});
 	},
 	getDataLoadedHandle: function(data){
@@ -108,4 +129,4 @@ ClassListModel.prototype = {
 };
 
 
-module.exports = ClassListModel;
+module.exports = HouseListModel;
