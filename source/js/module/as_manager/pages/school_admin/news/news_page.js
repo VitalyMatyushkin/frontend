b156233f7ddca2cd57265d820fe28f5cd4b3c873
Anatoly <atolmak@gmail.com>
2016-08-18 19:55:29 +0600

    news-view page added

--- source/js/module/as_manager/pages/school_admin/news/news_page.js
+++ source/js/module/as_manager/pages/school_admin/news/news_page.js
@@ -4,7 +4,8 @@ const	RouterView 			= require('module/core/router'),
 		Route 				= require('module/core/route'),
 		NewsListComponent 	= require("module/as_manager/pages/school_admin/news/news_list"),
 		NewsAddComponent 	= require("module/as_manager/pages/school_admin/news/news_add"),
-		NewsEditComponent 	= require("module/as_manager/pages/school_admin/news/news_edit");
+		NewsEditComponent 	= require("module/as_manager/pages/school_admin/news/news_edit"),
+		NewsViewComponent 	= require("module/as_manager/pages/school_admin/news/news-view");
 
 
 
@@ -20,6 +21,7 @@ const NewsPage = React.createClass({
 				<Route path="/school_admin/news" binding={binding.sub('newsList')} formBinding={binding.sub('classesForm')} component={NewsListComponent} />
 				<Route path="/school_admin/news/add"  binding={binding.sub('newsAdd')} component={NewsAddComponent} />
 				<Route path="/school_admin/news/edit" binding={binding.sub('newsForm')} component={NewsEditComponent}  />
+				<Route path="/school_admin/news/view" binding={binding.sub('newsView')} component={NewsViewComponent}  />
 			</RouterView>
 		)
 	}
