bdad90a444c4765ffe7707811496bee896f68452
Anatoly <atolmak@gmail.com>
2016-08-24 17:35:25 +0600

    Bug #1305 Parent calendar bug

--- source/js/module/as_parents/pages/events/calendar/all_challenges_list.js
+++ source/js/module/as_parents/pages/events/calendar/all_challenges_list.js
@@ -31,8 +31,10 @@ const AllChallengesList = React.createClass({
 			self._setFixturesByDate(descriptor.getCurrentValue().date);
 		});
 
-		binding.sub('activeChildId').addListener((descriptor) => {
-			descriptor.getCurrentValue() == 'all' && self.isMounted() && self.forceUpdate();
+		binding.sub('models').addListener(() => {
+			const currentCalendarDate = binding.toJS('calendar.selectDay');
+
+			currentCalendarDate && self._setFixturesByDate(currentCalendarDate.date);
 		});
 	},
 	_setFixturesByDate:function(date) {
