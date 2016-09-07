35b99b58f196a17a6a53c0db89c1cabe047e78ae
Anatoly <atolmak@gmail.com>
2016-09-07 17:22:32 +0600

    challenge_model comments

--- source/js/module/ui/challenges/challenge_model.js
+++ source/js/module/ui/challenges/challenge_model.js
@@ -7,6 +7,27 @@ const   DateHelper  = require('module/helpers/date_helper'),
 
 /**
  * This component contains the necessary information to render header or the event list.
+ * @class
+ * @param {object} event - event object
+ * @param {string} activeSchoolId - activeSchoolId
+ *
+ * @property {string} activeSchoolId - activeSchoolId
+ * @property {string} id - event id
+ * @property {string} name - event name
+ * @property {string} eventType - client event type ('inter-schools', 'houses' or 'internal')
+ * @property {boolean} isFinished - event is finished
+ * @property {boolean} isIndividualSport - event sport type is individual
+ * @property {string} sport - sport name
+ * @property {string} date - event start date
+ * @property {string} time - event start time
+ * @property {array} rivals - array of event rivals. rivals[0] - left, rivals[1] - right context
+ * @property {string} rivals[i].name - name team or player
+ * @property {string} rivals[i].from - from school or house name
+ * @property {string} rivals[i].schoolPic - school emblem
+ * @property {string} rivals[i].value - the combination of the 'name' and 'from'. Depending on the context(left, right),
+ * 				the type of спорт and the presence of an active school ID.
+ * @property {string} score - event score string, for example: "3 : 1"
+ *
  * */
 const ChallengeModel = function(event, activeSchoolId){
 	this.activeSchoolId = activeSchoolId;
