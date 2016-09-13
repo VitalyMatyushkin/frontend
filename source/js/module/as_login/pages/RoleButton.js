fc6dbd7b900b19e9a87cf65e17eaa84ab2a14e4d
Anatoly <atolmak@gmail.com>
2016-09-13 15:55:49 +0600

    fix role_list & user2

--- source/js/module/as_login/pages/RoleButton.js
+++ source/js/module/as_login/pages/RoleButton.js
@@ -2,13 +2,15 @@
  * Created by wert on 13.09.16.
  */
 
-const React = require('react');
+const 	React 		= require('react'),
+		RoleHelper 	= require('module/helpers/role_helper');
 
 /** Button to be rendered with role name */
 function RoleButton(props){
-	const 	onClick = props.onClick,
-			name	= props.name;
-	return <div className="bButton" onClick={() => onClick(name)}>{name}</div>;
+	const 	onClick 	= props.onClick,
+			name		= props.name,
+			clientName 	= RoleHelper.SERVER_ROLE_FOR_CLIENT[name];
+	return <div className="bButton" onClick={() => onClick(name)}>{clientName}</div>;
 }
 
 RoleButton.propTypes = {
