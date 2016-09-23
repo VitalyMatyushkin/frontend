5805aeae433dcbb12dde04135ec233ab69734e9b
Anatoly <atolmak@gmail.com>
2016-09-23 09:48:24 +0600

    undo remove files

---
+++ source/js/module/as_school/pages/opponents/list/list_page_mixin.js
@@ -0,0 +1,88 @@
+const 	React 		= require('react'),
+		Immutable 	= require('immutable');
+
+const ListPageMixin = {
+	propTypes: {
+		formBinding: React.PropTypes.any.isRequired,
+		filters: React.PropTypes.object
+	},
+	componentWillMount: function () {
+		var self = this,
+			binding = self.getDefaultBinding(),
+			globalBinding = self.getMoreartyContext().getBinding(),
+			activeSchoolId = globalBinding.get('activeSchoolId');
+
+		self.activeSchoolId = activeSchoolId;
+		self.updateData();
+	},
+	updateData: function(newFilter) {
+		var self = this,
+			requestFilter,
+			binding = self.getDefaultBinding(),
+			isFiltersActive = binding.meta().get('isFiltersActive');
+
+		self.request && self.request.cancel();
+
+		// Filtering by school
+		requestFilter = {
+
+		};
+
+		// add custom filter
+		if (typeof self.filters === 'object') {
+			Object.keys(self.filters).forEach(function (filter) {
+				requestFilter[filter] = self.filters[filter];
+			});
+		}
+
+		// Adding field filters if presented
+		if (newFilter && isFiltersActive && Object.keys(newFilter).length > 0) {
+			for (var filterName in newFilter) {
+				requestFilter.where[filterName] = newFilter[filterName];
+			}
+
+			self.lastFiltersState = newFilter;
+		}
+
+		self.request = window.Server.schoolOpponents.get(self.activeSchoolId).then(function (data) {
+			binding.set('list', Immutable.fromJS(data));
+		});
+	},
+	componentWillUnmount: function () {
+		var self = this;
+
+		self.request && self.request.cancel();
+	},
+	toggleFilters: function() {
+		var self = this,
+			metaBinding = self.getDefaultBinding().meta(),
+			isFiltersActive = metaBinding.get('isFiltersActive');
+
+		if (isFiltersActive) {
+			metaBinding.set('isFiltersActive', false);
+			self.updateData();
+		} else {
+			metaBinding.set('isFiltersActive', true);
+			self.updateData(self.lastFiltersState);
+		}
+	},
+	render: function() {
+		var self = this,
+			binding = self.getDefaultBinding(),
+			isFiltersActive = binding.meta().get('isFiltersActive');
+
+		return (
+			<div className="bSchoolMaster">
+				<div className={isFiltersActive ? 'bFiltersPage' : 'bFiltersPage mNoFilters'}>
+					<h1 className="eSchoolMaster_title">Opponents list
+					</h1>
+
+					{self.getTableView()}
+
+				</div>
+			</div>
+		)
+	}
+};
+
+module.exports = ListPageMixin;
---
+++ source/js/module/ui/fixtures/one_event.js
@@ -0,0 +1,100 @@
+const 	DateTimeMixin 	= require('module/mixins/datetime'),
+		If 				= require('module/ui/if/if'),
+		Morearty		= require('morearty'),
+		React 			= require('react');
+
+const OneEvent = React.createClass({
+	mixins: [Morearty.Mixin, DateTimeMixin],
+	_getScore: function(participantId) {
+		var self = this,
+			binding = self.getDefaultBinding(),
+			fixture = binding.toJS(),
+			score;
+
+		if (!participantId || !fixture) {
+			return null;
+		}
+
+		if (fixture.result && fixture.result.summary && fixture.result.summary.byTeams) {
+			score = fixture.result.summary.byTeams[participantId];
+		}
+
+		return (score === undefined ? '0' : score);
+	},
+	_getRivelNode: function(participan) {
+		var self = this,
+			pictures,
+			color,
+			name;
+
+		if (!participan) {
+			return '?';
+		}
+
+		pictures = participan.house && participan.house.pic || participan.school && participan.school.pic;
+		name = participan.house && participan.house.name || participan.school && participan.school.name;
+
+
+		// Внутреннее событие
+		if (participan.name) {
+			name = participan.name;
+		}
+
+		if (participan.house) {
+			pictures = participan.house.pic;
+
+			if (participan.house.colors && participan.house.colors[0]) {
+				color = participan.house.colors[0];
+			}
+		}
+
+		return (
+			<div className="eOneEvent_playerData">
+				<div className="eOneEvent_playerPic"><If condition={pictures}><img src={pictures}/></If><If condition={color && !pictures}><div className="eOneEvent_playerColor" style={{backgroundColor: color}}></div></If></div>
+				<div className="eOneEvent_playerName">{name}</div>
+				<div className="eOneEvent_result">{self._getScore(participan.id)}</div>
+			</div>
+		);
+
+	},
+	render: function() {
+		var self = this,
+			binding = self.getDefaultBinding(),
+			fixture = binding.toJS();
+
+		if (!fixture || !fixture.participants) {
+			return null;
+		}
+
+		return (
+			<div className="bOneEvent">
+				<div className="eOneEvent_name">Event results</div>
+
+				<div className="eOneEvent_info">
+					<div classNameName="eOneEvent_hours">Date and time: {self.getDateFromIso(fixture.startTime) + ' ' + self.getTimeFromIso(fixture.startTime)}</div>
+					<div classNameName="eOneEvent_sportsName">Sport: {fixture.sport.name}</div>
+				</div>
+
+				<div className="eOneEvent_players">
+					<div className="eOneEvent_player mLeft">
+						{self._getRivelNode(fixture.participants[0])}
+					</div>
+					<div className="eOneEvent_player mRight">
+						{self._getRivelNode(fixture.participants[1])}
+					</div>
+				</div>
+
+				<If condition={fixture.album}>
+					<div>
+						<div className="eOneEvent_name">Event album</div>
+						<EventAlbums binding={binding} />
+					</div>
+				</If>
+
+			</div>
+		)
+	}
+});
+
+
+module.exports = OneEvent;
