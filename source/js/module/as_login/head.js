cb9b3e174aef1fadd2c3a8ee442ecda99dad0689
Vitaly <heavyvetal88@mail.ru>
2016-09-23 15:50:40 +0600

    Bug #1423

--- source/js/module/as_login/head.js
+++ source/js/module/as_login/head.js
@@ -2,19 +2,26 @@
  * Created by wert on 16.01.16.
  */
 
-const 	React 	    = require('react'),
-        Morearty    = require('morearty');
+const React = require('react'),
+    Morearty = require('morearty');
 
 
 const Head = React.createClass({
     mixins: [Morearty.Mixin],
-
-    render: function() {
-        return (
-            <div className="bTopPanel">
-                <div className="bTopLogo">SquadInTouch</div>
-            </div>
-        )
+    returnToLoginPage: function () {
+        document.location.hash = '#login';
+    },
+    render: function () {
+        var self = this;
+        if (document.location.hash != '#login') {
+            return (
+                <div className="bTopPanel">
+                    <div className="bTopLogo" onClick={self.returnToLoginPage}><img src="images/logo.png"/></div>
+                </div>
+            )
+        }else{
+            return (<div></div>)
+        }
     }
 });
 
