4cce6de4978a29d63c952a0002baeee28999d1e6
Oleg Pikhnenko <mtbstreet91@gmail.com>
2016-09-26 19:04:44 +0600

    1457 was done

--- source/js/module/shared_pages/users/user_edit_tabDetails.js
+++ source/js/module/shared_pages/users/user_edit_tabDetails.js
@@ -4,6 +4,7 @@
 const   React       = require('react'),
         Immutable   = require('immutable'),
         Morearty    = require('morearty'),
+		Promise     = require('bluebird'),
         Form        = require('module/ui/form/form'),
         FormColumn 	= require('module/ui/form/form_column'),
         FormField   = require('module/ui/form/form_field');
@@ -30,12 +31,33 @@ const TabItemDetails = React.createClass({
         binding.clear('form');
     },
     _onSubmit:function(data){
-        var self = this,
-            binding = self.getDefaultBinding();
-        window.Server.user.put(self.params,data).then(function(user){
-            binding.set('popup',false);
-            return user;
-        });
+        const	self	= this,
+            	binding	= self.getDefaultBinding();
+
+		const promises = [];
+
+		// update verification fields by separate methods if these were changed
+		const oldData = binding.sub('form').toJS();
+		if(oldData.verification.status.email !== data.verification.status.email) {
+			promises.push(
+				window.Server.usersForceEmailVefication.put(self.params, {status: data.verification.status.email})
+			);
+		}
+		if(oldData.verification.status.sms !== data.verification.status.sms) {
+			promises.push(
+				window.Server.usersForcePhoneVefication.put(self.params, {status: data.verification.status.sms})
+			);
+		}
+
+		promises.push(
+			window.Server.user.put(self.params,data)
+				.then(user => {
+					binding.set('popup',false);
+					return user;
+				})
+		);
+
+		return Promise.all(promises);
     },
     getPhone: function(phone) {
         // return '7' + phone.replace('(', '').replace(')', '').replace('-', '');
@@ -58,6 +80,7 @@ const TabItemDetails = React.createClass({
     render:function(){
         var self = this,
             binding = self.getDefaultBinding();
+
         return (
             <div className="bDetails">
                 <Form binding={binding.sub('form')} service="superadmin/users" onSubmit={self._onSubmit} defaultButton="Save" formStyleClass="mDetails">
@@ -70,6 +93,8 @@ const TabItemDetails = React.createClass({
 						<FormField type="radio" field="gender" sourcePromise={self.getGender}>Gender</FormField>
                         <FormField type="text" field="email" validation="email server">Email</FormField>
                         <FormField type="phone" field="phone" validation="server" onPrePost={self.getPhone}>Mobile phone</FormField>
+						<FormField type="checkbox" field="verification.status.email">Email verified</FormField>
+						<FormField type="checkbox" field="verification.status.sms">Phone verified</FormField>
                     </FormColumn>
                 </Form>
             </div>
