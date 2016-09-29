7549dc624d328521f11da171d2136c44dfa9f510
Anatoly <atolmak@gmail.com>
2016-09-29 20:34:20 +0600

    Bug #1468 My first admin role

--- source/js/module/shared_pages/head/role_list.js
+++ source/js/module/shared_pages/head/role_list.js
@@ -103,7 +103,7 @@ const  RoleList = React.createClass({
 		const 	self 	= this,
 				binding = self.getDefaultBinding();
 
-		window.Server.publicSchools.get().then(schools => {
+		window.Server.publicSchools.get({filter:{limit:1000}}).then(schools => {
 			binding.set('schools', Immutable.fromJS(schools));
 		});
 	},
@@ -186,9 +186,7 @@ const  RoleList = React.createClass({
 					<div className={classNames({bRoles:true, mOpen:listOpen})} tabIndex="-1" ref="role_list" onBlur={self.onBlur}>
 						<div onClick={self.onToggle}>
 							{self.renderActiveRole()}
-							<div className="eArrow eCombobox_button">
-
-							</div>
+							<div className="eArrow eCombobox_button"></div>
 						</div>
 						<div className="eRolesList">
 							<div className="eScrollList">
