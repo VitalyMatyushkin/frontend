6d7117dfe9e93cfc4cae92e16db721c05c76729c
Anatoly <atolmak@gmail.com>
2016-10-03 19:22:44 +0600

    add comment

--- source/js/module/ui/form/types/dropdown.js
+++ source/js/module/ui/form/types/dropdown.js
@@ -1,23 +1,27 @@
 /**
  * Created by Anatoly on 03.10.2016.
  */
+
 const   React       = require('react'),
 		Morearty    = require('morearty'),
 		TypeMixin   = require('module/ui/form/types/type_mixin');
 
+/**
+ * The select list form component
+ * @param {array} options - array of options (string or {value, text} object)
+ * */
 const TypeDropDown = React.createClass({
 	mixins:[Morearty.Mixin, TypeMixin],
 	propTypes:{
 		options:React.PropTypes.array.isRequired
 	},
-	componentWillMount:function(){
-
-	},
 	renderOptions:function(){
 		return this.props.options.map(function(item, i){
 			if(typeof item === 'string')
+				/** string option */
 				return <option key={item+'-'+i} value={item}>{item}</option>;
 
+			/** {value, text} object */
 			return <option key={item.value+'-'+i} value={item.value}>{item.text}</option>;
 		});
 
