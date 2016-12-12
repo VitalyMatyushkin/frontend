332a500382cb9126431bb2b48d648d4de2cdeb93
Vitaly <heavyvetal88@mail.ru>
2016-12-12 18:05:48 +0600

    Bug #1894

--- source/js/module/ui/fixtures/fixture_title.js
+++ source/js/module/ui/fixtures/fixture_title.js
@@ -6,13 +6,13 @@ const React = require('react');
 
 function FixtureTitle(){
 	return (
-		<div className="eChallenge_title">
+		<div className="eChallenge_title mFixtures">
 			<span className="eChallengeDate_date">Date</span>
 			<div className="bChallenge mTitle">
 				<span className="eChallenge_hours">Time</span>
 				<span className="eChallenge_sport">Sport</span>
 				<span className="eChallenge_event">Event Name</span>
-				<span className="eChallenge_in">Game Type</span>
+				<span className="eChallenge_rivals">Game Type</span>
 				<span className="eChallenge_results">Score</span>
 			</div>
 		</div>
