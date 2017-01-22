08e0f992903341c2bd1718755565668685d51584
Oleg Pikhnenko <mtbstreet91@gmail.com>
2017-01-22 19:41:04 +0600

    INTERMEDIATE COMMIT. Add school to school union popup was done.

--- source/js/module/as_manager/pages/school_unions_pages/school_union_admin/pages/school_union_school_list/school_union_school_list.js
+++ source/js/module/as_manager/pages/school_unions_pages/school_union_admin/pages/school_union_school_list/school_union_school_list.js
@@ -2,20 +2,56 @@ const	React 						= require('react'),
 		Morearty					= require('morearty'),
 		Grid 						= require('module/ui/grid/grid'),
 		SchoolUnionSchoolListModel	= require('./school_union_school_list_model'),
+		AddSchoolPopup				= require('./add_school_popup'),
 		SchoolUnionSchoolListStyle	= require('../../../../../../../../styles/ui/b_school_union_school_list.scss');
 
 const SchoolUnionSchoolList = React.createClass({
 	mixins: [Morearty.Mixin],
 	componentWillMount: function () {
-		this.model = new SchoolUnionSchoolListModel(this, this.handleClickAddButton);
+		this.initData();
 	},
+	getIsOpenAddSchoolPopup: function() {
+		return this.getDefaultBinding().toJS('isAddSchoolPopupOpen');
+	},
+	setIsOpenAddSchoolPopup: function(isOpen) {
+		this.getDefaultBinding().set('isAddSchoolPopupOpen', isOpen);
+	},
+	/**
+	 * Function initialize data for this component.
+	 * @returns {*|any}
+	 */
+	initData: function() {
+		this.model = new SchoolUnionSchoolListModel(this, this.handleClickAddButton);
+
+		this.setIsOpenAddSchoolPopup(false);
+	},
+	/**
+	 * Handler for click event of add school button
+	 */
 	handleClickAddButton: function() {
-		console.log("I FUCK YOUR MOTHER");
+		this.setIsOpenAddSchoolPopup(true);
+	},
+	handleClickOkButton: function(school) {
+		console.log(school);
+		this.setIsOpenAddSchoolPopup(false);
+	},
+	/**
+	 * Handler for click event of close button from AddSchoolPopup
+	 */
+	handleClickCancelButton: function() {
+		this.setIsOpenAddSchoolPopup(false);
+	},
+	renderGrid: function() {
+		return typeof this.model.grid !== 'undefined' ? <Grid model={this.model.grid}/> : null;
 	},
 	render: function() {
 		return (
 			<div className="bSchoolUnionSchoolList">
-				{this.model.grid ? <Grid model={this.model.grid}/> : null}
+				{this.renderGrid()}
+				<AddSchoolPopup	isOpen					= {this.getIsOpenAddSchoolPopup()}
+								handleClickOkButton		= {this.handleClickOkButton}
+								handleClickCancelButton	= {this.handleClickCancelButton}
+				/>
 			</div>
 		);
 	}
