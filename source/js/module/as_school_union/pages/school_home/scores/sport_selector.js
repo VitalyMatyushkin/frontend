ad481ffa920c6aef13f74b874565edaaa47ad98f
Oleg Pikhnenko <mtbstreet91@gmail.com>
2017-01-28 22:14:38 +0600

    Perfect

---
+++ source/js/module/as_school_union/pages/school_home/scores/sport_selector.js
@@ -0,0 +1,56 @@
+const	React		= require('react'),
+		Morearty	= require('morearty'),
+		Immutable	= require('immutable');
+
+const Scores = React.createClass({
+	mixins: [Morearty.Mixin],
+	componentWillMount: function() {
+		this.initCurrentSport();
+	},
+	initCurrentSport: function() {
+		const sports = this.getDefaultBinding().toJS('sports');
+
+		if(sports.length === 0) {
+			this.getDefaultBinding().set('currentSport', undefined);
+		} else if(sports.length > 0) {
+			this.getDefaultBinding().set('currentSport', Immutable.fromJS(sports[0]));
+		}
+	},
+	getCurrentSport: function() {
+		return this.getDefaultBinding().toJS('currentSport');
+	},
+	getSports: function() {
+		const sports = this.getDefaultBinding().toJS('sports');
+
+		return sports.map(sport => {
+			return (
+				<option	value	= { sport.id }
+						key		= { sport.id }
+				>
+					{sport.name}
+				</option>
+			);
+		});
+	},
+	handleChangeSport: function(eventDescriptor) {
+		const	sportId	= eventDescriptor.target.value,
+				sports	= this.getDefaultBinding().toJS('sports');
+
+		const	foundSport	= sports.find(sport => sport.id === sportId);
+
+		this.getDefaultBinding().set('currentSport', Immutable.fromJS(foundSport));
+	},
+	render: function(){
+		return (
+			<select	className		= "bDropdown"
+					defaultValue	= "not-selected-sport"
+					value			= {this.getCurrentSport().id}
+					onChange		= {this.handleChangeSport}
+			>
+				{this.getSports()}
+			</select>
+		);
+	}
+});
+
+module.exports = Scores;
