67a6ca12179fbdc9d5c2c6ec93ea83e007743631
Woland <woland.rtf@gmail.com>
2017-02-06 18:06:05 +0600

    add parent role as an exception when render role list

--- source/js/module/shared_pages/settings/account/add_request.js
+++ source/js/module/shared_pages/settings/account/add_request.js
@@ -2,115 +2,119 @@
  * Created by Anatoly on 21.04.2016.
  */
 
-const   React       = require('react'),
-        Morearty    = require('morearty'),
-        Immutable   = require('immutable'),
+const	React		= require('react'),
+		Morearty	= require('morearty'),
+		Immutable	= require('immutable'),
 
-        Form        = require('module/ui/form/form'),
-        FormField 	= require('module/ui/form/form_field'),
-        classNames  = require('classnames'),
-        roleList    = require('module/data/roles_data');
+		Form		= require('module/ui/form/form'),
+		FormField 	= require('module/ui/form/form_field'),
+		classNames	= require('classnames'),
+		roleList	= require('module/data/roles_data');
 
 const AddPermissionRequest = React.createClass({
-    mixins:[Morearty.Mixin],
-    propTypes: {
-        onSuccess:          React.PropTypes.func,
-		onCancel: 			React.PropTypes.func
-    },
-    getDefaultState:function() {
-        return Immutable.Map({
-            preset:         '',
-            schoolId:       '',
-            comment:        ''
-        });
-    },
-    componentWillUnmount:function(){
-        this.getDefaultBinding().clear();
-    },
-    continueButtonClick:function(model){
-        const self = this;
-        if(model.studentName)
-            model.comment = `Request to be parent of [ ${model.studentName} ] \r\n` + model.comment;
+	mixins:[Morearty.Mixin],
+	propTypes: {
+		onSuccess:	React.PropTypes.func,
+		onCancel: 	React.PropTypes.func
+	},
+	getDefaultState:function() {
+		return Immutable.Map({
+			preset:		'',
+			schoolId:	'',
+			comment:	''
+		});
+	},
+	componentWillUnmount:function(){
+		this.getDefaultBinding().clear();
+	},
+	continueButtonClick:function(model){
+		const self = this;
+		if(model.studentName)
+			model.comment = `Request to be parent of [ ${model.studentName} ] \r\n` + model.comment;
 
-        window.Server.profileRequests.post(model)
-            .then(function(result){
-                return self.props.onSuccess && self.props.onSuccess(result);
-            });
-    },
-    isSchoolSelected: function() {
-        const binding = this.getDefaultBinding();
+		window.Server.profileRequests.post(model)
+			.then(function(result){
+				return self.props.onSuccess && self.props.onSuccess(result);
+			});
+	},
+	isSchoolSelected: function() {
+		const binding = this.getDefaultBinding();
 
-        return binding.meta().toJS('schoolId.value') !== '';
-    },
-    getSchoolSelectedId: function() {
-        const binding = this.getDefaultBinding();
+		return binding.meta().toJS('schoolId.value') !== '';
+	},
+	getSchoolSelectedId: function() {
+		const binding = this.getDefaultBinding();
 
-        return binding.meta().toJS('schoolId.value');
-    },
-    getPlaceHolderForRoleSelect: function() {
-        return this.isSchoolSelected() ? 'Please select role' : "";
-    },
-    isRoleSelectDisabled: function() {
-        return !this.isSchoolSelected();
-    },
-    getRoles: function() {
-        // user roles for active school
-        const currentRoles = this.getMoreartyContext().getBinding().toJS('userData.roleList.permissions')
-            .filter(p => p.schoolId === this.getSchoolSelectedId())
-            .map(p => p.role.toLowerCase());
+		return binding.meta().toJS('schoolId.value');
+	},
+	getPlaceHolderForRoleSelect: function() {
+		return this.isSchoolSelected() ? 'Please select role' : "";
+	},
+	isRoleSelectDisabled: function() {
+		return !this.isSchoolSelected();
+	},
+	getRoles: function() {
+		// user roles for active school
+		const currentRoles = this.getMoreartyContext().getBinding().toJS('userData.roleList.permissions')
+			.filter(p => p.schoolId === this.getSchoolSelectedId())
+			.map(p => p.role.toLowerCase());
+		// if user also have role in this school, we must cut this role from role list
+		// but this restriction don't act on parent
+		const hasUserCurrentRole = function(currentRole) {
+			if (currentRole !== 'parent') {
+				return currentRoles.find(role => role === currentRole);
+			} else {
+				return undefined;
+			}
+		};
 
-        const hasUserCurrentRole = function(currentRole) {
-            return currentRoles.find(r => r === currentRole);
-        };
+		return roleList.filter(role => !hasUserCurrentRole(role.id));
+	},
+	render: function() {
+		const 	binding		= this.getDefaultBinding(),
+				getSchools	= window.Server.publicSchools.filter,
+				isParent	= binding.meta('preset.value').toJS() === 'parent' && binding.meta('schoolId.value').toJS();
 
-        return roleList.filter(r => !hasUserCurrentRole(r.id));
-    },
-    render: function() {
-        const   self        = this,
-                binding     = self.getDefaultBinding(),
-                getSchools  = window.Server.publicSchools.filter,
-                isParent    = binding.meta('preset.value').toJS() === 'parent' && binding.meta('schoolId.value').toJS();
+		return (
+			<Form   name			= "New Request"
+					updateBinding	= { true }
+					binding			= { binding }
+					onSubmit		= { this.continueButtonClick }
+					onCancel		= { this.props.onCancel }
+					formStyleClass	= "bGrantContainer"
+					defaultButton	= "Submit"
+			>
+				<FormField type				= "autocomplete"
+						field				= "schoolId"
+						serviceFullData		= {getSchools}
+						placeholder 		= {'Please select school'}
+						validation			= "required"
+				>
+					School
+				</FormField>
 
-        return (
-            <Form   name            = "New Request"
-                    updateBinding   = { true }
-                    binding         = { binding }
-					onSubmit        = { self.continueButtonClick }
-					onCancel        = { self.props.onCancel }
-                    formStyleClass  = "bGrantContainer"
-                    defaultButton   = "Submit"
-            >
-                <FormField type             = "autocomplete"
-                           field            = "schoolId"
-                           serviceFullData  = {getSchools}
-						   placeholder 		= {'Please select school'}
-                           validation       = "required"
-                >
-                    School
-                </FormField>
-
-                <FormField type         = "select"
-                           field        = "preset"
-                           sourceArray  = {this.getRoles()}
-                           placeHolder  = {this.getPlaceHolderForRoleSelect()}
-                           isDisabled   = {this.isRoleSelectDisabled()}
-                >
-                    Role
-                </FormField>
-                <FormField type             = "text"
-                           field            = "studentName"
-                           fieldClassName   = {classNames({mHidden:!isParent})}
-                >
-                    Student
-                </FormField>
-                <FormField type         = "textarea"
-                           field        = "comment"
-                >
-                    Comment
-                </FormField>
-            </Form>
-        );
-    }
+				<FormField type			= "select"
+						   field		= "preset"
+						   sourceArray	= {this.getRoles()}
+						   placeHolder	= {this.getPlaceHolderForRoleSelect()}
+						   isDisabled	= {this.isRoleSelectDisabled()}
+				>
+					Role
+				</FormField>
+				<FormField type				= "text"
+						   field			= "studentName"
+						   fieldClassName	= {classNames({mHidden:!isParent})}
+				>
+					Student
+				</FormField>
+				<FormField type		= "textarea"
+						   field	= "comment"
+				>
+					Comment
+				</FormField>
+			</Form>
+		);
+	}
 });
 
 module.exports = AddPermissionRequest;
