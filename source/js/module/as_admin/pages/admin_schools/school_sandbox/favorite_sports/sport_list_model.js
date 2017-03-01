d2832c491e8420b487c3489ddaf16c7724f609f2
Oleg Pikhnenko <mtbstreet91@gmail.com>
2017-03-01 19:34:03 +0600

    2361 issue, code style fix.

--- source/js/module/as_admin/pages/admin_schools/school_sandbox/favorite_sports/sport_list_model.js
+++ source/js/module/as_admin/pages/admin_schools/school_sandbox/favorite_sports/sport_list_model.js
@@ -1,7 +1,4 @@
-const	React			= require('react'),
-		Morearty		= require('morearty'),
-		SVG				= require('module/ui/svg'),
-		Sport			= require('module/ui/icons/sport_icon'),
+const	Sport			= require('module/ui/icons/sport_icon'),
 		DataLoader		= require('module/ui/grid/data-loader'),
 		GridModel		= require('module/ui/grid/grid-model');
 
@@ -13,22 +10,22 @@ const SportListModel = function(page){
 
 	this.setColumns();
 	this.grid = new GridModel({
-		actionPanel:{
-			title		:'Sports',
-			showStrip	:true
+		actionPanel: {
+			title: 'Sports',
+			showStrip: true
 		},
-		columns:this.columns,
-		filters:{limit: 20}
+		columns: this.columns,
+		filters: {limit: 20}
 	});
 
-	const	globalBinding	= this.getMoreartyContext().getBinding(),
-			schoolId		= globalBinding.get('routing.pathParameters.0');
+	const globalBinding = this.getMoreartyContext().getBinding(),
+		schoolId = globalBinding.get('routing.pathParameters.0');
 
 	this.dataLoader = new DataLoader({
-		serviceName	: 'schoolSports',
-		params		: {schoolId:schoolId},
-		grid		: this.grid,
-		onLoad		: this.getDataLoadedHandle()
+		serviceName: 'schoolSports',
+		params: {schoolId:schoolId},
+		grid: this.grid,
+		onLoad: this.getDataLoadedHandle()
 	});
 
 };
@@ -55,26 +52,26 @@ SportListModel.prototype.getQuickEditAction = function(itemId, action){
 };
 
 SportListModel.prototype.getSelectAsFavoriteFunction = function(itemId){
-	const	globalBinding	= this.getMoreartyContext().getBinding(),
-			schoolId		= globalBinding.get('routing.pathParameters.0'),
-			sportId			= itemId;
+	const globalBinding = this.getMoreartyContext().getBinding(),
+		schoolId = globalBinding.get('routing.pathParameters.0'),
+		sportId = itemId;
 
 	window.Server.schoolSport.put({
-		schoolId	: schoolId,
-		sportId		: sportId
+		schoolId: schoolId,
+		sportId: sportId
 	}, {
 		isFavorite: true
 	}).then(() => this.dataLoader.loadData());
 };
 
 SportListModel.prototype.getUnselectAsFavoriteFunction = function(itemId){
-	const	globalBinding	= this.getMoreartyContext().getBinding(),
-			schoolId		= globalBinding.get('routing.pathParameters.0'),
-			sportId			= itemId;
+	const globalBinding = this.getMoreartyContext().getBinding(),
+		schoolId = globalBinding.get('routing.pathParameters.0'),
+		sportId = itemId;
 
 	window.Server.schoolSport.put({
-		schoolId	: schoolId,
-		sportId		: sportId
+		schoolId: schoolId,
+		sportId: sportId
 	}, {
 		isFavorite: false
 	}).then(() => this.dataLoader.loadData());
@@ -83,60 +80,59 @@ SportListModel.prototype.getUnselectAsFavoriteFunction = function(itemId){
 SportListModel.prototype.setColumns = function(){
 	this.columns = [
 		{
-			cell:{
-				type:'custom',
-				typeOptions:{
+			cell: {
+				type: 'custom',
+				typeOptions: {
 					parseFunction: item => <Sport name={item.name} className='bIcon_invites ' />
 				}
 			}
 		},
 		{
-			text:'Name',
-			isSorted:true,
-			cell:{
+			text: 'Name',
+			isSorted: true,
+			cell: {
 				dataField:'name'
 			},
-			filter:{
+			filter: {
 				type:'string'
 			}
 		},
 		{
-			text:'Favorite',
-			isSorted:true,
+			text: 'Favorite',
+			isSorted: true,
 			cell:{
-				dataField:'isFavorite',
-				type:'custom',
-				typeOptions:{
+				dataField: 'isFavorite',
+				type: 'custom',
+				typeOptions: {
 					parseFunction: this.getFavoriteTeamContentForItem.bind(this)
 				}
 			},
-			filter:{
-				type:'string'
+			filter: {
+				type: 'string'
 			}
 		},
 		{
-			text:'Description',
-			isSorted:true,
-			cell:{
-				dataField:'description'
+			text: 'Description',
+			isSorted: true,
+			cell: {
+				dataField: 'description'
 			},
-			filter:{
-				type:'string'
+			filter: {
+				type: 'string'
 			}
 		},
 		{
-			text:'Actions',
-			cell:{
-				type:'action-list',
-				typeOptions:{
-					getActionList	: this.getActions.bind(this),
-					actionHandler	: this.getQuickEditAction.bind(this)
+			text: 'Actions',
+			cell: {
+				type: 'action-list',
+				typeOptions: {
+					getActionList: this.getActions.bind(this),
+					actionHandler: this.getQuickEditAction.bind(this)
 				}
 			}
 		}
 	];
 };
-
 
 SportListModel.prototype.getFavoriteTeamContentForItem = function(item) {
 	if (item.isFavorite) {
@@ -153,7 +149,7 @@ SportListModel.prototype.getFavoriteTeamContentForItem = function(item) {
 SportListModel.prototype.getDataLoadedHandle = function(){
 	const binding = this.getDefaultBinding();
 
-	return function(data){
+	return (data) => {
 		binding.set('data', this.grid.table.data);
 	};
 };
