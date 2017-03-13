bc494568a24653992277bf36b772fde0fac2c093
Oleg Pikhnenko <mtbstreet91@gmail.com>
2017-03-13 21:48:10 +0600

    some refactoring

--- source/js/module/ui/managers/team_bundle.js
+++ source/js/module/ui/managers/team_bundle.js
@@ -1,33 +1,36 @@
+// Main components
 const	React				= require('react'),
-		TeamChooser			= require('./../teamChooser'),
-		TeamWrapper			= require('./team_wrapper'),
-		classNames			= require('classnames'),
-		TeamHelper			= require('module/ui/managers/helpers/team_helper'),
-		If					= require('module/ui/if/if'),
 		Immutable			= require('immutable'),
-		Morearty            = require('morearty'),
-		MoreartyHelper		= require('module/helpers/morearty_helper');
+		Morearty			= require('morearty'),
+		classNames			= require('classnames');
 
-const TeamModeView = React.createClass({
+// Team bundle react components
+const	TeamChooser			= require('./teamChooser'),
+		TeamWrapper			= require('./team_wrapper');
+
+// Helpers
+const	TeamHelper			= require('module/ui/managers/helpers/team_helper');
+
+const TeamBundle = React.createClass({
 	mixins: [Morearty.Mixin],
 	componentWillMount: function() {
-		const	self	= this;
+		const self = this;
 
-		self._initBinding();
+		self.initBinding();
 		self.addListeners();
 	},
-	_initBinding: function() {
+	initBinding: function() {
 		const	self	= this;
 
-		self._initTeamWrapperFiltersBinding();
+		self.initTeamWrapperFiltersBinding();
 	},
-	_initTeamWrapperFiltersBinding: function() {
+	initTeamWrapperFiltersBinding: function() {
 		const	self	= this,
 				rivals	= self.getBinding().rivals.toJS();
 
-		rivals.forEach((rival, index) => self._initTeamWrapperFilterBinding(index, rival));
+		rivals.forEach((rival, index) => self.initTeamWrapperFilterBinding(index, rival));
 	},
-	_initTeamWrapperFilterBinding: function(rivalIndex, rival) {
+	initTeamWrapperFilterBinding: function(rivalIndex, rival) {
 		const	self	= this,
 				binding	= self.getDefaultBinding();
 
@@ -63,7 +66,7 @@ const TeamModeView = React.createClass({
 		// add players from one team to blacklist of other team
 		binding.sub(`teamWrapper.${index}.___teamManagerBinding.teamStudents`).addListener(descriptor => {
 			binding.set(
-				`teamWrapper.${self._getAnotherRivalIndex(index)}.___teamManagerBinding.blackList`,
+				`teamWrapper.${self.getAnotherRivalIndex(index)}.___teamManagerBinding.blackList`,
 				descriptor.getCurrentValue()
 			)
 		});
@@ -73,7 +76,7 @@ const TeamModeView = React.createClass({
 	 * @param teamId
 	 * @private
 	 */
-	_onTeamClick: function(teamId, team) {
+	onTeamClick: function(teamId, team) {
 		const	self	= this,
 				binding	= self.getDefaultBinding();
 
@@ -81,19 +84,19 @@ const TeamModeView = React.createClass({
 				prevSelectedTeamId	= binding.toJS(`teamWrapper.${rivalIndex}.selectedTeamId`);
 
 		if(prevSelectedTeamId !== teamId) {
-			self._selectTeam(teamId, team);
+			self.selectTeam(teamId, team);
 		}
 	},
-	_getAnotherRivalIndex: function(rivalIndex) {
+	getAnotherRivalIndex: function(rivalIndex) {
 
 		return rivalIndex === 0 ? 1 : 0;
 	},
-	_selectTeam: function(teamId, team) {
+	selectTeam: function(teamId, team) {
 		const	self	= this,
 				binding	= self.getDefaultBinding();
 
 		const	rivalIndex			= binding.toJS('selectedRivalIndex'),
-				anotherRivalIndex	= self._getAnotherRivalIndex(rivalIndex);
+				anotherRivalIndex	= self.getAnotherRivalIndex(rivalIndex);
 
 		binding
 			.atomically()
@@ -131,12 +134,12 @@ const TeamModeView = React.createClass({
 			)
 			.commit();
 	},
-	_deselectTeam: function() {
+	deselectTeam: function() {
 		const	self	= this,
 				binding	= self.getDefaultBinding();
 
 		const	rivalIndex			= binding.toJS('selectedRivalIndex'),
-				anotherRivalIndex	= self._getAnotherRivalIndex(rivalIndex);
+				anotherRivalIndex	= self.getAnotherRivalIndex(rivalIndex);
 
 		binding
 			.atomically()
@@ -170,7 +173,7 @@ const TeamModeView = React.createClass({
 			)
 			.commit();
 	},
-	_renderTeamWrapper: function() {
+	renderTeamWrapper: function() {
 		const	self				= this,
 				binding				= self.getDefaultBinding(),
 				selectedRivalIndex	= binding.toJS('selectedRivalIndex'),
@@ -180,14 +183,14 @@ const TeamModeView = React.createClass({
 											model				: self.getBinding().model,
 											rival				: self.getBinding().rivals.sub(0),
 											players				: binding.sub(`players.${0}`),
-											otherTeamPlayers	: binding.sub(`players.${self._getAnotherRivalIndex(0)}`),
+											otherTeamPlayers	: binding.sub(`players.${self.getAnotherRivalIndex(0)}`),
 											error				: self.getBinding('error').sub(0)
 										},{
 											default:			binding.sub(`teamWrapper.${1}`),
 											model:				self.getBinding().model,
 											rival:				self.getBinding().rivals.sub(1),
 											players:			binding.sub(`players.${1}`),
-											otherTeamPlayers:	binding.sub(`players.${self._getAnotherRivalIndex(1)}`),
+											otherTeamPlayers:	binding.sub(`players.${self.getAnotherRivalIndex(1)}`),
 											error				: self.getBinding('error').sub(1)
 										}];
 
@@ -224,19 +227,8 @@ const TeamModeView = React.createClass({
 		const	self	= this,
 				binding	= self.getDefaultBinding();
 
-		self._deselectTeam();
+		self.deselectTeam();
 		binding.set(`teamTable.${rivalIndex}.isSelectedTeam`, Immutable.fromJS(false));
-	},
-	_renderErrorBox: function() {
-		const	selectedRivalIndex	= this.getDefaultBinding().toJS('selectedRivalIndex'),
-				_errorText			= this.getBinding('error').toJS(selectedRivalIndex).text;
-
-		// it doesn't show team name warning
-		return (
-			<div className="eTeam_errorBox">
-				{_errorText !== 'Please enter team name' ? _errorText : ''}
-			</div>
-		);
 	},
 	render: function() {
 		const	self	= this,
@@ -265,13 +257,13 @@ const TeamModeView = React.createClass({
 			//Need to go TeamChooser on react state, delete morearty
 			//problem is in my ugly realization of TeamChooser component
 			//some data init on componentWillMount function
-			//so we can't just send new data to TeamChooser in some point of TeamChooser lifecycle and
-			//and hope - everything will work good.
-			//NO!) All fall down, man. Sorrrry.
+			//so we can't just send new data to TeamChooser in some point of TeamChooser lifecycle
+			//and hope - everything will work good. NO!)
+			//All fall down. Sorrrry, mate.
 			teamChoosers = teamTableBindings.map((binding, index) =>
 				<TeamChooser	key				= { `team-chooser-${index}` }
-								onTeamClick		= { self._onTeamClick }
-								onTeamDeselect	= { self._deselectTeam }
+								onTeamClick		= { self.onTeamClick }
+								onTeamDeselect	= { self.deselectTeam }
 								binding			= { teamTableBindings[index] }
 								isEnable		= { parseInt(selectedRivalIndex, 10) === index }
 				/>
@@ -281,10 +273,10 @@ const TeamModeView = React.createClass({
 		return (
 			<div>
 				{ teamChoosers }
-				{ self._renderTeamWrapper() }
+				{ self.renderTeamWrapper() }
 			</div>
 		);
 	}
 });
 
-module.exports = TeamModeView;
+module.exports = TeamBundle;
