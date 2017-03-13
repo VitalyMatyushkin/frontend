92c47aeb4b12a21913ded6f4af2ec40b80ffb279
Woland <woland.rtf@gmail.com>
2017-03-13 20:30:14 +0600

    fix superadmin summary page

--- source/js/module/as_admin/pages/admin_schools/school_sandbox/summary/summary_page.js
+++ source/js/module/as_admin/pages/admin_schools/school_sandbox/summary/summary_page.js
@@ -1,13 +1,13 @@
 const	React 					= require('react'),
-		Immutable					= require('immutable'),
-		SummaryComponent	= require('module/as_admin/pages/admin_schools/school_sandbox/summary/summary_component'),
+		Immutable				= require('immutable'),
+		SummaryComponent		= require('module/as_admin/pages/admin_schools/school_sandbox/summary/summary_component'),
 		Morearty				= require('morearty');
 
 const SummaryPage = React.createClass({
 	mixins: [Morearty.Mixin],
 
 	render: function() {
-		const binding				= this.getDefaultBinding(),
+		const 	binding				= this.getDefaultBinding(),
 				school					= binding.toJS('schoolDetails');
 
 				return <SummaryComponent school={school} />
