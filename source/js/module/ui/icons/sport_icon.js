bd166d91768bd1be97ba4b42965d266b6b351097
Woland <woland.rtf@gmail.com>
2017-03-13 13:29:40 +0600

    new sport icon

--- source/js/module/ui/icons/sport_icon.js
+++ source/js/module/ui/icons/sport_icon.js
@@ -3,39 +3,43 @@
  * Created by Anatoly on 02.03.2016.
  */
 
-const   React   = require('react'),
-        SVG     = require('module/ui/svg');
+const 	React 	= require('react'),
+		SVG 	= require('module/ui/svg');
 
 /** Renders sport icon as SVG: ball, bat, and so on */
 const SportIcon = React.createClass({
-    propTypes: {
-        name:       React.PropTypes.string.isRequired,
+	propTypes: {
+		name: 		React.PropTypes.string.isRequired,
 		title:		React.PropTypes.string,
-        className:  React.PropTypes.string
-    },
+		className: 	React.PropTypes.string
+	},
 	/**
 	 * Picking proper sport icon by sport name
 	 * @param sport {string} sport name
 	 * @returns {string} style name to apply on SVG
 	 */
-    getSportIcon:function(sport: string): string {
-        let icon;
-        sport = sport ? sport.trim().toLowerCase() : '';
-        switch (true){
-            case 	sport === 'football' ||
-					sport === 'futsal' ||
-					sport === 'football 5-a-side' ||
+	getSportIcon:function(sport: string): string {
+		let icon;
+		sport = sport ? sport.trim().toLowerCase() : '';
+		switch (true){
+			case 	sport === 'football' ||
 					sport === 'football indoor':
-                icon = "ball";
-                break;
+				icon = "ball";
+				break;
+			case 	sport === 'futsal':
+				icon = "footsal";
+				break;
+			case sport === 'football 5-a-side':
+				icon = "football_five";
+				break;
 			case 	sport === 'rugby union' ||
 					sport === 'tag rugby':
 				icon = "rugby_union";
 				break;
-			case sport === 'rugby sevens':
+			case 	sport === 'rugby sevens':
 				icon = "rugby_sevens";
 				break;
-			case sport === 'rugby tens':
+			case 	sport === 'rugby tens':
 				icon = "rugby_tens";
 				break;
 			case 	sport === 'sprint (60 m)' ||
@@ -68,10 +72,10 @@ const SportIcon = React.createClass({
 					sport === 'table tennis doubles':
 				icon = "table_tennis";
 				break;
-			case sport === 'high 5 netball':
-				icon = "netball";
+			case 	sport === 'high 5 netball':
+				icon = "netball_five";
 				break;
-			case sport === 'hockey indoor':
+			case 	sport === 'hockey indoor':
 				icon = "hockey_indoor";
 				break;
 			case 	sport === 'chess (individual)' ||
@@ -87,40 +91,65 @@ const SportIcon = React.createClass({
 				icon = "squash";
 				break;
 			case 	sport === 'golf (individual)' ||
-					sport === 'golf (team)' ||
-					sport === 'tri golf (individual)' ||
-					sport === 'tri golf (team)':
+					sport === 'golf (team)':
 				icon = "golf";
+				break;
+			case 	sport === 'tri golf (individual)' ||
+					sport === 'tri golf (team)':
+				icon = "tri_golf";
 				break;
 			case 	sport === 'orienteering (individual)' ||
 					sport === 'orienteering (team)' ||
 					sport === 'orienteering':
 				icon = "orienteering";
 				break;
+			case 	sport === 'american football':
+				icon = "american_football";
+				break;
 			//no icon
-			case 	sport === 'swimming' ||
-					sport === 'volleyball' ||
-					sport === 'dodgeball' ||
-					sport === 'sports hall athletics' ||
-					sport === 'handball' ||
-					sport === 'american football':
+			case 	sport === 'sports hall athletics':
 				icon = "empty";
 				break;
-            default:
-                icon = sport;
-                break;
-        }
-        return ("icon_" + icon);
-    },
-    render:function(){
-        const icon = this.getSportIcon(this.props.name);
+			//maybe
+			case 	sport === 'hammer throw':
+				icon = "hammer_throw";
+				break;
+			case 	sport === 'water polo':
+				icon = "water polo";
+				break;
+			case 	sport === 'mini golf':
+				icon = "mini_golf";
+				break;
+			case 	sport === 'javelin throw':
+				icon = "javelin_throw";
+				break;
+			case 	sport === 'discus throw':
+				icon = "discus_throw";
+				break;
+			case 	sport === 'shot put':
+				icon = "shot_put";
+				break;
+			case 	sport === 'long jump':
+				icon = "long_jump";
+				break;
+			case 	sport === 'high jump':
+				icon = "high_jump";
+				break;
+			default:
+				icon = sport;
+				break;
+		}
+		return ("icon_" + icon);
+	},
+	render:function(){
+		const icon = this.getSportIcon(this.props.name);
 
-        if(!!this.props.name) {
-            return <SVG title={this.props.title} classes={this.props.className} icon={icon} />;
-        } else {
-            return null;
-        }
-    }
+		if(!!this.props.name) {
+			return <SVG title={this.props.title} classes={this.props.className} icon={icon} />;
+		} else {
+			return null;
+		}
+	}
 });
 
 module.exports = SportIcon;
