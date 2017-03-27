592d15c581ef671878cdb8d3c69c50d40ccb9135
Oleg Pikhnenko <mtbstreet91@gmail.com>
2017-03-27 18:00:22 +0600

    2472 issue fix.

--- source/js/module/ui/form/form_element_manager.js
+++ source/js/module/ui/form/form_element_manager.js
@@ -1,10 +1,12 @@
 const	React					= require('react'),
-		Button					= require('../button/button');
+		Button					= require('../button/button'),
+		classNames				= require('classnames');
 
 const	FormElementManagerStyle	= require('../../../../styles/ui/forms/b_form_element_manager.scss');
 
 const FormElementManager = React.createClass({
 	propTypes: {
+		isVisible:	React.PropTypes.bool.isRequired,
 		type:		React.PropTypes.string.isRequired,
 		text:		React.PropTypes.string.isRequired,
 		onClick:	React.PropTypes.func.isRequired
@@ -16,8 +18,13 @@ const FormElementManager = React.createClass({
 		};
 	},
 	render: function () {
+		const className = classNames({
+			'bFormElementManager':	true,
+			'mInvisible':			!this.props.isVisible
+		});
+
 		return (
-			<div className="bFormElementManager">
+			<div className={className}>
 				<Button
 					onClick				= {this.props.onClick}
 					text				= {this.props.text}
