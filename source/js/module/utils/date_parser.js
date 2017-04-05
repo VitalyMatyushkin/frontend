edf06fe519e942701aa690ec9d76fc615148d931
Yaroslav Shcherban <yar.shcherban@gmail.com>
2017-04-05 17:09:04 +0300

    dateparser added

---
+++ source/js/module/utils/date_parser.js
@@ -0,0 +1,22 @@
+const   moment = require('moment'),
+        assert = require('assert');
+
+/**
+ * Creates Date Object for date as input string
+ * @param {string} date
+ * @returns {Date} recognized date
+ */
+const getBdayDateFormat = (date) => {
+    assert(typeof date === 'string', 'date is not a string');
+
+    moment.parseTwoDigitYear = (input) => {
+        return parseInt(input) + (parseInt(input) > 54 ? 1900 : 2000);
+    };
+
+    const formatDate = moment(date, ["DD/MM/YYYY", "DD/MM/YY", "DD/M/YY", "DD.MM.YYYY", "DD.MM.YY", "DD.M.YY", "YYYY-MM-DD", "DD MMM YYYY"]);
+
+    return formatDate.isValid() ? formatDate.toDate() : undefined;
+};
+
+
+module.exports = getBdayDateFormat;
