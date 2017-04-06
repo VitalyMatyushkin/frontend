abdbb7e0158de099ef99761a7932c4596b965234
Alex Povar <a.v.povar@gmail.com>
2017-04-06 23:36:40 +0600

    Fixed date parsing

--- source/js/module/utils/date_parser.js
+++ source/js/module/utils/date_parser.js
@@ -1,21 +1,28 @@
+// @flow
+
 const   moment = require('moment');
 
 const thisYear = moment().year();	// for example, 2017
 
 /**
  * Parses string to Date taking in account fact that there is no any single user in system who born in future.
- * @param {string} date
+ * @param {string} str
  * @returns {Date} recognized date or undefined if date cannot be parsed
  */
-const getBdayDateFormat = (date) => {
-    const	parsedDate	= moment(date, ["DD/MM/YYYY", "DD/MM/YY", "DD/M/YY", "DD.MM.YYYY", "DD.MM.YY", "DD.M.YY", "YYYY-MM-DD", "DD MMM YYYY"]),
-    		parsedYear	= parsedDate.year();
+const getBdayDateFormat = function(str: string): ?Date {
+    const	parsedDate	= moment(str, ["DD/MM/YYYY", "DD/MM/YY", "DD/M/YY", "DD.MM.YYYY", "DD.MM.YY", "DD.M.YY", "YYYY-MM-DD", "DD MMM YYYY"]);
 
-    if(parsedYear > thisYear) {				// rolling back 100 years if date in future. 2018 -> 1918
-		parsedDate.year(parsedYear - 100);
+    if(parsedDate.isValid() === true) {
+		const parsedYear	= parsedDate.year();
+
+		if(parsedYear > thisYear) {				// rolling back 100 years if date in future. 2018 -> 1918
+			parsedDate.year(parsedYear - 100);
+		}
+
+		return parsedDate.toDate();
+	} else {
+    	return undefined;
 	}
-
-    return parsedDate.isValid() ? parsedDate.toDate() : undefined;
 };
 
 
