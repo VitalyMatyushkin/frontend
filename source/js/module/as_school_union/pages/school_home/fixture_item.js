67b52c42596f840dfd88a0353812c488a8c13e25
Woland <woland.rtf@gmail.com>
2017-04-26 17:34:54 +0600

    Fix errors in accordance with review

--- source/js/module/as_school_union/pages/school_home/fixture_item.js
+++ source/js/module/as_school_union/pages/school_home/fixture_item.js
@@ -80,7 +80,9 @@ const FixtureItem = React.createClass({
 				  */
 				activeSchoolId		= event.inviterSchoolId,
 				challengeModel		= new ChallengeModel(event, activeSchoolId),
-				isAwaitingOpponent	= event.status === 'INVITES_SENT';
+				isAwaitingOpponent	= event.status === 'INVITES_SENT',
+				score 				= challengeModel.sport.toLowerCase() === 'cricket' ? challengeModel.textResult : challengeModel.score,
+				scoreText 			= challengeModel.sport.toLowerCase() === 'cricket' ? '' : 'Score';
 
 		return (
 			<div className="bFixtureContainer">
@@ -98,8 +100,8 @@ const FixtureItem = React.createClass({
 							{this.renderLeftOpponentSide(event, challengeModel)}
 							<div className="eFixture_item mResult">
 								<div>
-									<div className="bFix_scoreText">{isAwaitingOpponent ? 'Awaiting opponent' : 'Score'}</div>
-									<div className="bFix_scoreResult">{isAwaitingOpponent ? '' : `${challengeModel.score}`}</div>
+									<div className="bFix_scoreText">{isAwaitingOpponent ? 'Awaiting opponent' : scoreText}</div>
+									<div className="bFix_scoreResult">{isAwaitingOpponent ? '' : `${score}`}</div>
 								</div>
 							</div>
 							{this.renderRightOpponentSide(event, challengeModel)}
