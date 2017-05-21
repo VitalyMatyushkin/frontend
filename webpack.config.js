0057e7726ceb7d7bf50c0529a384622f479d3b75
Alex Povar <a.v.povar@gmail.com>
2017-05-21 16:31:46 +0300

    Fat-free frontend with sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -74,8 +74,12 @@ module.exports = {
 			}
 		],
 	},
+	devtool: 'source-map',
 	plugins: [
-		new webpack.optimize.UglifyJsPlugin(),
+		new webpack.optimize.UglifyJsPlugin({
+			mangle:		false,	// I'm not sure if mangling can be enabled safely. So disabling it for a while
+			sourceMap:	true
+		}),
 		new ExtractTextPlugin({
 			filename: 'styles.css',
 			allChunks: true
