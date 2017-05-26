4c62606b927d99824c8a04133f8388b3643c127c
Oleg Pikhnenko <mtbstreet91@gmail.com>
2017-05-26 17:49:13 +0300

    fix login

--- source/js/module/ui/login/user/form.js
+++ source/js/module/ui/login/user/form.js
@@ -36,6 +36,11 @@ const LoginUserForm = React.createClass({
 
 		return (
 			<div className="bLoginForm">
+				<RememberMeCheckbox
+					filed		= {'rememberMe'}
+					isChecked	= { binding.toJS('rememberMe') }
+					onChange	= { this.onChangeRememberMe }
+				/>
 				<Form	name				= {this.tmpFormName}
 						service				= {Auth.login}
 						binding				= {this.getDefaultBinding()}
@@ -62,11 +67,6 @@ const LoginUserForm = React.createClass({
 						validation="required"
 						binding={binding}
 						id="login_password"
-					/>
-					<RememberMeCheckbox
-						filed		= {'rememberMe'}
-						isChecked	= { binding.toJS('rememberMe') }
-						onChange	= { this.onChangeRememberMe }
 					/>
 				</Form>
 				<If condition={this.props.customName === 'default'}>
