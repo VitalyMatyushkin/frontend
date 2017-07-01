e5fd5ab883ea6008bb7ca71cf2126d847c233420
Oleg Pikhnenko <mtbstreet91@gmail.com>
2017-07-01 17:52:00 +0600

    2903 issue. was done.

---
+++ source/js/module/as_manager/pages/event/view/rivals/helpers/rivals_helper.js
@@ -0,0 +1,183 @@
+const	propz		= require('propz'),
+		TeamHelper	= require('module/ui/managers/helpers/team_helper'),
+		EventHelper	= require('module/helpers/eventHelper');
+
+const RivalsHelper = {
+	getRivalsByEvent: function(activeSchoolId, viewMode, event) {
+		const eventType = event.eventType;
+
+		let rivals = [];
+
+		if(TeamHelper.isTeamSport(event)) {
+			if(EventHelper.clientEventTypeToServerClientTypeMapping['inter-schools'] === eventType) {
+				const	schoolsData	= event.schoolsData,
+						teamsData	= event.teamsData;
+
+				// iterate all schools
+				schoolsData.forEach(school => {
+					const rival = {};
+
+					rival.school = school;
+					let schoolInvite = undefined;
+					if (typeof event.invites !== 'undefined') {
+						schoolInvite = event.invites.find(invite => invite.invitedSchoolId === school.id);
+					}
+
+					if(typeof schoolInvite !== 'undefined') {
+						rival.invite = schoolInvite;
+					}
+
+					// search all teams for current school
+					teamsData.forEach(t => {
+						if(t.schoolId === school.id) {
+							rival.team = t;
+						}
+					});
+
+					rivals.push(rival);
+				});
+
+				rivals = rivals.sort((rival1, rival2) => {
+					if(rival1.school.id === activeSchoolId && rival2.school.id !== activeSchoolId) {
+						return -1;
+					}
+					if(rival1.school.id !== activeSchoolId && rival2.school.id === activeSchoolId) {
+						return 1;
+					}
+
+					return 0;
+				});
+			} else if(EventHelper.clientEventTypeToServerClientTypeMapping['houses'] === eventType) {
+				const	schoolsData	= event.schoolsData,
+						housesData	= event.housesData,
+						teamsData	= event.teamsData;
+
+				// iterate all schools
+				housesData.forEach(house => {
+					const rival = {};
+
+					rival.school = schoolsData[0];
+					rival.house = house;
+					// search all teams for current house
+					teamsData.forEach(t => {
+						if(t.houseId === house.id) {
+							rival.team = t;
+						}
+					});
+
+					rivals.push(rival);
+				});
+
+				rivals = rivals.sort((rival1, rival2) => {
+					if(rival1.house.name < rival2.house.name) {
+						return -1;
+					}
+					if(rival1.house.name > rival2.house.name) {
+						return 1;
+					}
+
+					return 0;
+				});
+			} else if(EventHelper.clientEventTypeToServerClientTypeMapping['internal'] === eventType) {
+				const	schoolsData	= event.schoolsData,
+						teamsData	= event.teamsData;
+
+				// iterate all schools
+				teamsData.forEach(team => {
+					const rival = {};
+
+					rival.school = schoolsData[0];
+					rival.team = team;
+
+					rivals.push(rival);
+				});
+
+				rivals = rivals.sort((rival1, rival2) => {
+					if(rival1.team.name < rival2.team.name) {
+						return -1;
+					}
+					if(rival1.team.name > rival2.team.name) {
+						return 1;
+					}
+
+					return 0;
+				});
+			}
+		} else if (TeamHelper.isIndividualSport(event)){
+			if (EventHelper.clientEventTypeToServerClientTypeMapping['inter-schools'] === eventType) {
+				if (viewMode === 'general') {
+					const	schoolsData	= event.schoolsData,
+							players		= event.individualsData,
+							scores		= propz.get(event, ['results', 'individualScore']);
+					// iterate all schools
+					schoolsData.forEach(school => {
+						const rival = {};
+
+						rival.school = school;
+						let schoolInvite = undefined;
+						if (typeof event.invites !== 'undefined') {
+							schoolInvite = event.invites.find(invite => invite.invitedSchoolId === school.id);
+						}
+
+						if(typeof schoolInvite !== 'undefined') {
+							rival.invite = schoolInvite;
+						}
+
+						// search all players for current school
+						rival.players = [];
+						players.forEach( player => {
+							const playerScoreObject = scores.find(score => score.userId === player.userId);
+
+							if (typeof playerScoreObject !== 'undefined') {
+								const	playerScore			= propz.get(playerScoreObject, ['score']),
+										playerExtraScore	= propz.get(playerScoreObject, ['richScore', 'points']);
+
+								player.score = playerScore;
+								player.extraScore = playerExtraScore;
+							} else {
+								player.score = 0;
+								player.extraScore = 0;
+							}
+
+							if(player.schoolId === school.id) {
+								rival.players.push(player);
+							}
+						});
+
+						rivals.push(rival);
+					});
+				} else {
+					const	rival	= {},
+							players	= event.individualsData,
+							scores	= propz.get(event, ['results', 'individualScore']);
+
+					rival.school = {};
+					rival.players = [];
+					players.forEach( player => {
+						const playerScoreObject = scores.find(score => score.userId === player.userId);
+
+						if (typeof playerScoreObject !== 'undefined') {
+							const	playerScore			= propz.get(playerScoreObject, ['score']),
+									playerExtraScore	= propz.get(playerScoreObject, ['richScore', 'points']);
+
+							player.score = playerScore;
+							player.extraScore = playerExtraScore;
+						} else {
+							player.score = 0;
+							player.extraScore = 0;
+						}
+
+						rival.players.push(player);
+
+					});
+
+					rivals.push(rival);
+				}
+			}
+		}
+
+		return rivals;
+	}
+};
+
+module.exports = RivalsHelper;
--- source/js/module/as_manager/pages/event/view/rivals/rivals.js
+++ source/js/module/as_manager/pages/event/view/rivals/rivals.js
@@ -5,6 +5,7 @@ const	React						= require('react'),
 		TeamHelper					= require('module/ui/managers/helpers/team_helper'),
 		EventHelper					= require('module/helpers/eventHelper'),
 		SportHelper					= require('module/helpers/sport_helper'),
+		RivalsHelper				= require('module/as_manager/pages/event/view/rivals/helpers/rivals_helper'),
 		InvitesMixin				= require('module/as_manager/pages/invites/mixins/invites_mixin'),
 		classNames					= require('classnames'),
 		propz 						= require('propz'),
@@ -31,220 +32,41 @@ const Rivals = React.createClass({
 		this.listeners.forEach(listener => this.getDefaultBinding().removeListener(listener));
 	},
 	componentWillMount: function() {
-		const binding = this.getDefaultBinding();
-
-		const	event		= binding.toJS('model'),
-				eventType	= event.eventType;
-
-		let	rivals		= [];
-
 		this.initViewMode();
 
+		const	binding		= this.getDefaultBinding();
+
+		const	event		= binding.toJS('model'),
+				eventType	= event.eventType,
+				viewMode	= binding.toJS('view_mode');
+
+		// Get main part of rivals
+		let		rivals		= RivalsHelper.getRivalsByEvent(this.props.activeSchoolId, viewMode, event);
+
+		// Additional preparation
 		if(TeamHelper.isTeamSport(event)) {
-			if(EventHelper.clientEventTypeToServerClientTypeMapping['inter-schools'] === eventType) {
-				const	schoolsData	= event.schoolsData,
-						teamsData	= event.teamsData;
-
-				// iterate all schools
-				schoolsData.forEach(school => {
-					const rival = {};
-
-					rival.school = school;
-					let schoolInvite = undefined;
-					if (typeof event.invites !== 'undefined') {
-						schoolInvite = event.invites.find(invite => invite.invitedSchoolId === school.id);
-					}
-					
-					if(typeof schoolInvite !== 'undefined') {
-						rival.invite = schoolInvite;
-					}
-
-					// search all teams for current school
-					teamsData.forEach(t => {
-						if(t.schoolId === school.id) {
-							rival.team = t;
-						}
-					});
-					rival.isTeamScoreWasChanged = false;
-					rival.isIndividualScoreAvailable = this.getInitValueForIsIndividualScoreAvailable(rival);
-					this.initResultsForRival(rival, event);
-
-					rivals.push(rival);
-				});
-
-				rivals = rivals.sort((rival1, rival2) => {
-					if(rival1.school.id === this.props.activeSchoolId && rival2.school.id !== this.props.activeSchoolId) {
-						return -1;
-					}
-					if(rival1.school.id !== this.props.activeSchoolId && rival2.school.id === this.props.activeSchoolId) {
-						return 1;
-					}
-
-					return 0;
-				});
-			} else if(EventHelper.clientEventTypeToServerClientTypeMapping['houses'] === eventType) {
-				const	schoolsData	= event.schoolsData,
-						housesData	= event.housesData,
-						teamsData	= event.teamsData;
-
-				// iterate all schools
-				housesData.forEach(house => {
-					const rival = {};
-
-					rival.school = schoolsData[0];
-					rival.house = house;
-					// search all teams for current house
-					teamsData.forEach(t => {
-						if(t.houseId === house.id) {
-							rival.team = t;
-						}
-					});
-					rival.isTeamScoreWasChanged = false;
-					rival.isIndividualScoreAvailable = this.getInitValueForIsIndividualScoreAvailable(rival);
-					this.initResultsForRival(rival, event);
-
-					rivals.push(rival);
-				});
-
-				rivals = rivals.sort((rival1, rival2) => {
-					if(rival1.house.name < rival2.house.name) {
-						return -1;
-					}
-					if(rival1.house.name > rival2.house.name) {
-						return 1;
-					}
-
-					return 0;
-				});
-			} else if(EventHelper.clientEventTypeToServerClientTypeMapping['internal'] === eventType) {
-				const	schoolsData	= event.schoolsData,
-						teamsData	= event.teamsData;
-
-				// iterate all schools
-				teamsData.forEach(team => {
-					const rival = {};
-
-					rival.school = schoolsData[0];
-					rival.team = team;
-
-					rival.isTeamScoreWasChanged = false;
-					rival.isIndividualScoreAvailable = this.getInitValueForIsIndividualScoreAvailable(rival);
-					this.initResultsForRival(rival, event);
-
-					rivals.push(rival);
-				});
-
-				rivals = rivals.sort((rival1, rival2) => {
-					if(rival1.team.name < rival2.team.name) {
-						return -1;
-					}
-					if(rival1.team.name > rival2.team.name) {
-						return 1;
-					}
-
-					return 0;
-				});
-			}
+			rivals.forEach(rival => {
+				rival.isTeamScoreWasChanged = false;
+				rival.isIndividualScoreAvailable = this.getInitValueForIsIndividualScoreAvailable(rival);
+				this.initResultsForRival(rival, event);
+			});
 		} else if (TeamHelper.isIndividualSport(event)){
 			if (EventHelper.clientEventTypeToServerClientTypeMapping['inter-schools'] === eventType) {
-				
-				if (binding.toJS('view_mode') === 'general') {
-					const	schoolsData	= event.schoolsData,
-							players 	= event.individualsData,
-							scores 		= propz.get(event, ['results', 'individualScore']);
-					// iterate all schools
-					schoolsData.forEach(school => {
-						const rival = {};
-						
-						rival.school = school;
-						let schoolInvite = undefined;
-						if (typeof event.invites !== 'undefined') {
-							schoolInvite = event.invites.find(invite => invite.invitedSchoolId === school.id);
-						}
-						
-						if(typeof schoolInvite !== 'undefined') {
-							rival.invite = schoolInvite;
-						}
-						
+				if (viewMode === 'general') {
+					rivals.forEach(rival => {
 						rival.isIndividualScoreAvailable = true;
 						rival.isTeamScoreWasChanged = false;
-						
-						// search all players for current school
-						rival.players = [];
-						players.forEach( player => {
-							const playerScoreObject = scores.find(score => score.userId === player.userId);
-							
-							if (typeof playerScoreObject !== 'undefined') {
-								const 	playerScore 		= propz.get(playerScoreObject, ['score']),
-										playerExtraScore 	= propz.get(playerScoreObject, ['richScore', 'points']);
-								
-								player.score = playerScore;
-								player.extraScore = playerExtraScore;
-							} else {
-								player.score = 0;
-								player.extraScore = 0;
-							}
-							
-							if(player.schoolId === school.id) {
-								rival.players.push(player);
-							}
-						});
-						
 						rival.score = this.getExtraScoreForRival(rival);
+
 						this.addListenerForChangeMode();
-						rivals.push(rival);
 					});
-					//If at least one opponent has extraScore then sort by extraScore else by activeSchoolId
-					const isExtraScore = rivals.some(rival => rival.score !== 0);
-					
-					if (isExtraScore) {
-						// Sort array of rivals by DESC of extraScores
-						rivals = rivals.sort((rival1, rival2) => {
-							return rival2.score - rival1.score;
-						});
-					} else {
-						// Sort array of rivals by activeSchoolId
-						rivals = rivals.sort((rival1, rival2) => {
-							if(rival1.school.id === this.props.activeSchoolId && rival2.school.id !== this.props.activeSchoolId) {
-								return -1;
-							}
-							if(rival1.school.id !== this.props.activeSchoolId && rival2.school.id === this.props.activeSchoolId) {
-								return 1;
-							}
-							return 0;
-						});
-					}
-					
 				} else {
-					const 	rival 	= {},
-							players = event.individualsData,
-							scores 	= propz.get(event, ['results', 'individualScore']);
-					
-					rival.school = {};
-					rival.players = [];
-					players.forEach( player => {
-						const playerScoreObject = scores.find(score => score.userId === player.userId);
-						
-						if (typeof playerScoreObject !== 'undefined') {
-							const 	playerScore 		= propz.get(playerScoreObject, ['score']),
-									playerExtraScore 	= propz.get(playerScoreObject, ['richScore', 'points']);
-							
-							player.score = playerScore;
-							player.extraScore = playerExtraScore;
-						} else {
-							player.score = 0;
-							player.extraScore = 0;
-						}
-						
-							rival.players.push(player);
-						
+					rivals.forEach(rival => {
+						rival.isIndividualScoreAvailable = true;
+						rival.isTeamScoreWasChanged = false;
+
+						this.addListenerForChangeMode();
 					});
-					
-					rival.isIndividualScoreAvailable = true;
-					rival.isTeamScoreWasChanged = false;
-					this.addListenerForChangeMode();
-					rivals.push(rival);
-					
 				}
 			}
 		}
