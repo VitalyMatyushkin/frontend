bfc06a8afeca5952406b825d45443b97440b6261
VitalyMatyushkin <matushkin.vitaliy@gmail.com>
2017-07-13 12:10:29 +0600

    fix arrow function and promise

--- source/js/module/test_api/application.js
+++ source/js/module/test_api/application.js
@@ -9,12 +9,12 @@ let logId = 0;
 const ApplicationView = React.createClass({
     getInitialState: function() {
         return {
-            inputValue: '6vdc5kr5quiwl67uryj83zykrjnsdjpuwo9vdp6x',
+            inputValue: 'eox9noqguyigu2f5tuu1c51qwxei0maf9wg8bbo2',
             logs: []
         };
     },
 
-    updateInputValue: function(event) {
+    updateInputValue: (event) => {
         this.setState({
             inputValue: event.target.value
         });
@@ -28,112 +28,137 @@ const ApplicationView = React.createClass({
 
 
     handleSubmit: function(event) {
-        let self = this; let selectedRole;
-        self.getProfile().then(function (res) {
-            self.addLog("Check profile: " + res.textStatus + ". User: " + res.data.firstName + " " + res.data.lastName);
-        });
-        self.getRoles().then(function (res) {
-            self.addLog("Check roles: " + res.textStatus);
-            return Promise.all(res.data.map(function (role) {
-                self.setRole(role.name).then(function (roles) {
-                    self.addLog("Switch role " + roles.data.role + ": " + roles.textStatus);
-                    self.getSchools(roles.data.key).then(function (schools) {
-                        self.addLog("Info about schools for " + role.name + " " + schools.textStatus);
-                        return Promise.all(schools.data.map(function (school) {
-                            if (roles.data.role === 'ADMIN' || roles.data.role === 'MANAGER' || roles.data.role === 'TRAINER') {
-                                self.getStudentList(roles.data.key, school.id).then(function (students) {
-                                    self.addLog("Student list " + school.name + " for " + roles.data.role + " " + students.textStatus);
-                                });
-                                self.getHouseList(roles.data.key, school.id).then(function (houses) {
-                                    self.addLog("House list " + school.name + " for " + roles.data.role + " " + houses.textStatus);
-                                });
-                                self.getFormList(roles.data.key, school.id).then(function (forms) {
-                                    self.addLog("Form list " + school.name + " for " + roles.data.role + " " + forms.textStatus);
-                                });
-                                self.getEvents(roles.data.key, school.id).then(function (events) {
-                                    self.addLog("Events " + school.name + " for " + roles.data.role + " " + events.textStatus);
-                                });
+        const self = this;
+        self.getProfile().then(() => {
+            return self.getRoles()
+        })
+        .then((roles) => {
+            return Promise.all(roles.data.map((role) => {
+                self.setRole(role.name)
+                .then((selectedRole) => {
+                    self.getSchools(selectedRole.data.key, selectedRole.data.role)
+                    .then((schools) => {
+                        return Promise.all(schools.data.map((school) => {
+                            if (selectedRole.data.role === 'ADMIN' || selectedRole.data.role === 'MANAGER' || selectedRole.data.role === 'TRAINER') {
+                                Promise.all([
+                                    self.getStudentList(selectedRole.data.key, school.id, school.name),
+                                    self.getHouseList(selectedRole.data.key, school.id, school.name),
+                                    self.getFormList(selectedRole.data.key, school.id, school.name)
+                                ]);
                             }
-                        }));
-                    });
+                            self.getEvents(selectedRole.data.key, school.id, school.name)
+                        }))
+                    })
                 })
-            }))
+            }));
+        })
+        .catch((err) => {
+            self.addLog(err);
         });
         event.preventDefault();
     },
 
     getProfile: function() {
-        const   usid = this.state.inputValue,
+        const   usid = this.state.inputValue, self = this,
                 url = "http://api.stage1.squadintouch.com/i/profile?filter=%22%22";
         return AJAX({
             url: url,
             type: 'GET',
             headers: {"usid": usid}
+        }).then((res) => {
+            self.addLog("Check profile: " + res.textStatus + ". User: " + res.data.firstName + " " + res.data.lastName);
+            return res;
         })
     },
 
     getRoles: function() {
-        const   usid = this.state.inputValue,
+        const   usid = this.state.inputValue, self = this,
                 url = "http://api.stage1.squadintouch.com/i/roles?filter=%22%22";
         return AJAX({
             url: url,
             type: 'GET',
             headers: {"usid": usid}
+        }).then((res) => {
+            self.addLog("Check roles: " + res.textStatus);
+            return res;
         })
     },
 
     setRole: function(role) {
-        const   usid = this.state.inputValue,
+        const   usid = this.state.inputValue, self = this,
                 url = "http://api.stage1.squadintouch.com/i/roles/" + role + "/become?filter=%22%22";
         return AJAX({
             url: url,
             type: 'POST',
             headers: {"usid": usid}
+        }).then((res) => {
+            self.addLog("Switch role " + res.data.role + ": " + res.textStatus);
+            return res;
         })
     },
 
-    getSchools: function(sessionKey) {
-        const   url = "http://api.stage1.squadintouch.com/i/schools?filter=%22%22";
+    getSchools: function(sessionKey, role) {
+        const   url = "http://api.stage1.squadintouch.com/i/schools?filter=%22%22",
+                self = this;
         return AJAX({
             url: url,
             type: 'GET',
             headers: {"usid": sessionKey}
+        }).then((res) => {
+            self.addLog("Info about schools for role: " + role + ": " + res.textStatus);
+            return res;
         })
     },
 
-    getStudentList: function(sessionKey, schoolId) {
-        const   url = "http://api.stage1.squadintouch.com/i/schools/" + schoolId + "/students?filter=%7B%22limit%22%3A20%2C%22skip%22%3A20%7D&{}";
+    getStudentList: function(sessionKey, schoolId, schoolName) {
+        const   url = "http://api.stage1.squadintouch.com/i/schools/" + schoolId + "/students?filter=%7B%22limit%22%3A20%2C%22skip%22%3A20%7D&{}",
+                self = this;
         return AJAX({
             url: url,
             type: 'GET',
             headers: {"usid": sessionKey}
+        }).then((res) => {
+            self.addLog("Student list for school " + schoolName + ": " + res.textStatus);
+            return res;
         })
     },
 
-    getHouseList: function(sessionKey, schoolId) {
-        const   url = "http://api.stage1.squadintouch.com/i/schools/" + schoolId + "/houses?filter=%7B%22limit%22%3A20%7D&{}";
+    getHouseList: function(sessionKey, schoolId, schoolName) {
+        const   url = "http://api.stage1.squadintouch.com/i/schools/" + schoolId + "/houses?filter=%7B%22limit%22%3A20%7D&{}",
+                self = this;
         return AJAX({
             url: url,
             type: 'GET',
             headers: {"usid": sessionKey}
+        }).then((res) => {
+            self.addLog("House list for school " + schoolName + ": " + res.textStatus);
+            return res;
         })
     },
 
-    getFormList: function(sessionKey, schoolId) {
-        const   url = "http://api.stage1.squadintouch.com/i/schools/" + schoolId + "/forms?filter=%7B%22limit%22%3A30%7D&{}";
+    getFormList: function(sessionKey, schoolId, schoolName) {
+        const   url = "http://api.stage1.squadintouch.com/i/schools/" + schoolId + "/forms?filter=%7B%22limit%22%3A30%7D&{}",
+                self = this;
         return AJAX({
             url: url,
             type: 'GET',
             headers: {"usid": sessionKey}
+        }).then((res) => {
+            self.addLog("Form list for school " + schoolName + ": " + res.textStatus);
+            return res;
         })
     },
 
-    getEvents: function(sessionKey, schoolId) {
-        const   url = "http://api.stage1.squadintouch.com/i/schools/" + schoolId + "/events";
+    getEvents: function(sessionKey, schoolId, schoolName) {
+        const   url = "http://api.stage1.squadintouch.com/i/schools/" + schoolId + "/events",
+                self = this;
         return AJAX({
             url: url,
             type: 'GET',
             headers: {"usid": sessionKey}
+        }).then((res) => {
+            self.addLog("Events for school " + schoolName + ": " + res.textStatus);
+            return res;
         })
     },
 
