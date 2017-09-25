2e26630f5a12649361068eea39bba9af1fa941b5
VitalyMatyushkin <matushkin.vitaliy@gmail.com>
2017-09-25 18:13:44 +0600

    add month filter

--- source/js/module/as_manager/pages/events/events_fixtures.js
+++ source/js/module/as_manager/pages/events/events_fixtures.js
@@ -2,32 +2,40 @@
  * Created by Anatoly on 22.09.2016.
  */
 
-const   React			= require('react'),
-        Morearty		= require('morearty'),
-		Immutable		= require('immutable'),
-		DateHelper 		= require('module/helpers/date_helper'),
-		EventHelper		= require('module/helpers/eventHelper'),
-		Fixtures 		= require('module/ui/fixtures/fixtures'),
+const   React		= require('react'),
+        Morearty	= require('morearty'),
+		Immutable	= require('immutable'),
+		DateHelper 	= require('module/helpers/date_helper'),
+		EventHelper	= require('module/helpers/eventHelper'),
+		MonthNavBar	= require('module/ui/calendar/month_year_selector'),
+		Fixtures 	= require('module/ui/fixtures/fixtures'),
 
 		FixturesStyles	= require('./../../../../../styles/ui/bFixtures.scss');
 
 const EventFixtures = React.createClass({
 	mixins: [Morearty.Mixin],
 	componentWillMount: function () {
-		const binding = this.getDefaultBinding();
+		const 	binding = this.getDefaultBinding(),
+				bindingCalendar = this.getBinding('calendar'),
+				currentDate = bindingCalendar.toJS('monthDate');
 
 		binding.clear();
 		this.activeSchoolId = this.getMoreartyContext().getBinding().get('userRules.activeSchoolId');
-
+		binding.set('dateCalendar', currentDate);
 		this._setEvents();
 	},
+	// getInitialState: function() {
+	// 	const 	binding = this.getBinding('calendar'),
+	// 			currentDate = binding.toJS('monthDate');
+	// 	return {
+	// 		dateState: currentDate
+	// 	};
+	// },
 	_setEvents: function() {
-		const binding = this.getBinding('calendar'),
-			currentDate = binding.toJS('monthDate');
-
+		const 	binding = this.getDefaultBinding();
 		this._setEventsByDateRange(
-			DateHelper.getStartDateTimeOfMonth(currentDate),
-			DateHelper.getEndDateTimeOfMonth(currentDate)
+			DateHelper.getStartDateTimeOfMonth(binding.get('dateCalendar')),
+			DateHelper.getEndDateTimeOfMonth(binding.get('dateCalendar'))
 		);
 	},
 	_setEventsByDateRange: function(gteDate, ltDate) {
@@ -56,12 +64,23 @@ const EventFixtures = React.createClass({
 	onClickChallenge: function (eventId) {
 		document.location.hash = 'event/' + eventId;
 	},
+	onMonthClick: function (date) {
+		const binding = this.getDefaultBinding();
+
+		binding.set('sync', false);
+		binding.set('dateCalendar', date);
+
+		this._setEvents();
+	},
 	render: function () {
-		const activeSchoolId	= this.getMoreartyContext().getBinding().get('userRules.activeSchoolId'),
+		const 	activeSchoolId	= this.getMoreartyContext().getBinding().get('userRules.activeSchoolId'),
 				binding			= this.getDefaultBinding();
 
 		return (
 			<div className="bFixtures">
+				<MonthNavBar date = {binding.get('dateCalendar')}
+							 onMonthClick = {date => this.onMonthClick(date)}
+				/>
 				<Fixtures	events			= {binding.toJS('models')}
 							activeSchoolId	= {activeSchoolId}
 							sync			= {binding.toJS('sync')}
