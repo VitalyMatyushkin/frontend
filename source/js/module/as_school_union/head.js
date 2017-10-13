9657e53e0f7dff3506df314e986e8e17693baccf
sollsollsollsolll <mtbstreet91@gmail.com>
2017-10-13 14:24:53 +0600

    codestyle

--- source/js/module/as_school_union/head.js
+++ source/js/module/as_school_union/head.js
@@ -1,30 +1,31 @@
-const 	Logo 		= require('module/as_school/head/logo'),
-		TopMenu 	= require('module/ui/menu/public_menu'),
-		Morearty    = require('morearty'),
-		React 		= require('react'),
-		Bootstrap  	= require('styles/bootstrap-custom.scss');
+const	Morearty	= require('morearty'),
+		React		= require('react');
+
+const	Logo		= require('module/as_school/head/logo'),
+		TopMenu		= require('module/ui/menu/public_menu');
+
+const	Bootstrap	= require('styles/bootstrap-custom.scss');
 
 const Head = React.createClass({
 	mixins: [Morearty.Mixin],
 	render: function() {
-		const 	self 	= this,
-				binding = this.getDefaultBinding();
-
-        return (
-            <div className="bTopPanel mSchoolPanel mFixed">
-                <div className="eTopPanel_container">
-                    <div className="eTopPanel_row">
-                        <div className="eTopPanel_col_small_4">
-                            <Logo />
-                        </div>
-                        <div className="eTopPanel_col_small_8 eTopPanel_right">
-                            <TopMenu menuItems={['Scores','Calendar','Fixtures','Results','News','Schools']}></TopMenu>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+		return (
+			<div className="bTopPanel mSchoolPanel mFixed">
+				<div className="eTopPanel_container">
+					<div className="eTopPanel_row">
+						<div className="eTopPanel_col_small_4">
+							<Logo />
+						</div>
+						<div className="eTopPanel_col_small_8 eTopPanel_right">
+							<TopMenu
+								menuItems={['Scores','Calendar','Fixtures','Results','News','Schools']}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
 });
 
 module.exports = Head;
