41741dfafa573ec22b833a9059083949903521a2
Oleg Pikhnenko <mtbstreet91@gmail.com>
2017-10-26 17:15:48 +0600

    consent request chat issue (#2125)

--- source/js/module/as_manager/pages/parents_pages/event/event_page.js
+++ source/js/module/as_manager/pages/parents_pages/event/event_page.js
@@ -3,10 +3,17 @@ const	React			= require('react'),
 
 		Event			= require('../../event/event');
 
+const EventFormConsts = require('module/as_manager/pages/events/manager/event_form/consts/consts');
+
 const EventPage = React.createClass({
 	mixins: [Morearty.Mixin],
 	componentWillMount: function () {
 		this.activeSchoolId = this.getActiveSchoolId();
+	},
+	getDefaultProps: function() {
+		return {
+			mode: EventFormConsts.EVENT_FORM_MODE.SCHOOL
+		};
 	},
 	getActiveSchoolId: function() {
 		return this.getMoreartyContext().getBinding().get('routing.parameters.schoolId');
@@ -16,9 +23,10 @@ const EventPage = React.createClass({
 				binding	= self.getDefaultBinding();
 
 		return (
-			<Event	binding					= {binding}
-					isShowControlButtons	= {false}
-					activeSchoolId			= {this.activeSchoolId}
+			<Event	binding					= { binding }
+					isShowControlButtons	= { false }
+					activeSchoolId			= { this.activeSchoolId }
+					mode					= { this.props.mode }
 			/>
 		);
 	}
