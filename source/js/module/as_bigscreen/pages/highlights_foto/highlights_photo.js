1deaadbdb27050628ad93ceeac14996e91c42b25
Oleg Pikhnenko <mtbstreet91@gmail.com>
2017-11-17 16:13:54 +0300

    big screen tv issue. test1. (#2188)

--- source/js/module/as_bigscreen/pages/highlights_foto/highlights_photo.js
+++ source/js/module/as_bigscreen/pages/highlights_foto/highlights_photo.js
@@ -1,45 +1,54 @@
-const React = require('react'),
-		BigScreenActions	= require('./../../actions/BigScreenActions');
+const	React				= require('react'),
+	BigScreenActions	= require('./../../actions/BigScreenActions');
+
+const Style = require('styles/ui/bid_screen_fixtures/bHighlightsPhoto.scss');
 
 const HighlightsPhoto = React.createClass({
 
 	propTypes: {
 		photos: React.PropTypes.array.isRequired
 	},
-	
-	render: function() {
-		const 	photos			= this.props.photos,
-				defaultPhoto 	= '//images.squadintouch.com/images/3txvxdlvjkcm65e13re3nbeuoyoft8pwnhd3_1478107508550.jpg',	// hack not to brake page when there is no photos. TODO: fix me
-				photo0Url		= photos[0] ? photos[BigScreenActions.getRandomPhotoIndex(photos)].picUrl : defaultPhoto,
-				photo1Url		= photos[1] ? photos[BigScreenActions.getRandomPhotoIndex(photos)].picUrl : defaultPhoto,
-				photo2Url		= photos[2] ? photos[BigScreenActions.getRandomPhotoIndex(photos)].picUrl : defaultPhoto;
 
-		const styles = [
-			{
-				backgroundImage: `url(${window.Server.images.getResizedToHeightUrl(photo0Url, 300)})`
-			},
-			{
-				backgroundImage: `url(${window.Server.images.getResizedToHeightUrl(photo1Url, 300)})`
-			},
-			{
-				backgroundImage: `url(${window.Server.images.getResizedToHeightUrl(photo2Url, 300)})`
-			}
+	defaultPhoto: '//images.squadintouch.com/images/3txvxdlvjkcm65e13re3nbeuoyoft8pwnhd3_1478107508550.jpg',
+
+	getPicUrlByOrder: function () {
+		const photos = this.props.photos;
+
+		const currentPic = photos[BigScreenActions.getRandomPhotoIndex(photos)];
+		let picUrl = typeof currentPic !== 'undefined' ? currentPic.picUrl : this.defaultPhoto;
+
+		return picUrl;
+	},
+	render: function() {
+		const currentPicUrlArray = [
+			window.Server.images.getResizedToHeightUrl(
+				this.getPicUrlByOrder(0),
+				300
+			),
+			window.Server.images.getResizedToHeightUrl(
+				this.getPicUrlByOrder(1),
+				300
+			),
+			window.Server.images.getResizedToHeightUrl(
+				this.getPicUrlByOrder(2),
+				300
+			)
 		];
 
 		return (
 			<div className="bHighlightsPhoto">
-				<div	className	= "eHighlightsPhoto_Photo"
-						style		= {styles[0]}
+				<img	className	= "eHighlightsPhoto_img"
+						src			= {currentPicUrlArray[0]}
 				>
-				</div>
-				<div	className	= "eHighlightsPhoto_smallPhoto"
-						style		= {styles[1]}
+				</img>
+				<img	className	= "eHighlightsPhoto_img mSmall"
+						src			= {currentPicUrlArray[1]}
 				>
-				</div>
-				<div	className	= "eHighlightsPhoto_smallPhoto"
-						style		= {styles[2]}
+				</img>
+				<img	className	= "eHighlightsPhoto_img mSmall"
+						src			= {currentPicUrlArray[2]}
 				>
-				</div>
+				</img>
 			</div>
 		);
 	}
