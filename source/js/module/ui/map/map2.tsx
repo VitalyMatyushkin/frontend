e7663e56b7e3ecd6ab8cddff44be8529ac1951a0
Woland <woland.rtf@gmail.com>
2017-11-29 22:45:46 +0600

    map component in typescript done

---
+++ source/js/module/ui/map/map2.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+
+export interface Point{
+	lat?: number,
+	lng?: number
+}
+
+export interface MapProps{
+	point: Point,
+	customStylingClass?: string
+}
+
+export class Map extends React.Component<MapProps, {}> {
+	mapView: 		any;
+	mapBounds: 		any;
+	points: 		any;
+
+	componentDidMount(): void {
+		const {point} = this.props;
+		this.setUpMap(point);
+	}
+	componentWillReceiveProps(nextProps: MapProps): void {
+		if(
+			this.props.point && nextProps.point &&
+			(this.props.point.lat != nextProps.point.lat || this.props.point.lng != nextProps.point.lng)
+		) {
+			this.setUpMap(nextProps.point);
+		}
+	}
+	setUpMap(pointGoogle: Point): void {
+		const 	mapNode = this.refs['map'],
+				mapCenter = new (window as any).google.maps.LatLng(pointGoogle.lat, pointGoogle.lng),
+				mapOptions = {
+					center: mapCenter,
+					zoom: 16,
+					disableDefaultUI: true
+				};
+		this.mapView = new (window as any).google.maps.Map(mapNode, mapOptions);
+
+		this.mapBounds = new (window as any).google.maps.LatLngBounds();
+		this.mapBounds.extend(mapCenter);
+
+		this.points = {};
+		this.points['school'] = new (window as any).google.maps.Marker({
+			position: mapCenter,
+			map: this.mapView
+		});
+	}
+
+	render() {
+		const classNames = 'bMapView '+ (typeof this.props.customStylingClass !== 'undefined' ? this.props.customStylingClass : '');
+
+		return (
+			<div className={classNames} ref="map">
+			</div>
+		)
+	}
+}
