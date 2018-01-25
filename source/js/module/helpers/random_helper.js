a70b8952861efc4d6c39adedb9d3e2303af9e74b
Oleg Pikhnenko <mtbstreet91@gmail.com>
2018-01-25 15:23:07 +0600

    Fix random. (#2383)

--- source/js/module/helpers/random_helper.js
+++ source/js/module/helpers/random_helper.js
@@ -1,7 +1,7 @@
 const RandomHelper = {
 	getRandomString: function() {
 		// just current date in timestamp view
-		return String(+ new Date()) + String(this.getRandomInt(10, 100));
+		return String(+ new Date()) + String(this.getRandomInt(10, 1000000));
 	},
 	getRandomInt: function(min, max) {
 		return Math.floor(Math.random() * (max - min)) + min;
