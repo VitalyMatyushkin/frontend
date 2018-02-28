9c9003278b8ae2da9d4d913c5fa46768966cafea
VitalyMatyushkin <matushkin.vitaliy@gmail.com>
2018-02-28 22:38:40 +0600

    count sessions for user (#2518)

--- source/js/module/as_admin/pages/admin_add/user/form.js
+++ source/js/module/as_admin/pages/admin_add/user/form.js
@@ -7,16 +7,10 @@ const 	Form 		= require('module/ui/form/form'),
 const UserForm = React.createClass({
 	mixins: [Morearty.Mixin],
 	propTypes: {
-		onSuccess: React.PropTypes.func,
 		otherService:React.PropTypes.string
 	},
-	onSuccess: function(data) {
-		var self = this;
-		self.showForm = false;
-	},
 	getGender: function() {
-		const 	self = this,
-			gendersArray = [
+		const gendersArray = [
 				{
 					value: 'Male',
 					id: 'MALE'
@@ -29,17 +23,29 @@ const UserForm = React.createClass({
 
 		return Promise.resolve(gendersArray);
 	},
+	_onSubmit:function(data){
+		data.countOneTimeIosSessions = Number(data.countOneTimeIosSessions);
+		data.countOneTimeAndroidSessions = Number(data.countOneTimeAndroidSessions);
+		data.countOneTimeWebSessions = Number(data.countOneTimeWebSessions);
+
+		data.verified = {email:true, personal:true, phone:true};
+
+		window.Server.users.post(data)
+		.then(() => {document.location.hash = 'users'});
+	},
 	render: function() {
-		var self = this,
-			binding = self.getDefaultBinding();
+		const binding = this.getDefaultBinding();
 		return (
-			<Form name="Create New User" updateBinding={true} service="superadmin/users" binding={binding} onSuccess={self.props.onSuccess}>
+			<Form name="Create New User" updateBinding={true} service="superadmin/users" onSubmit={this._onSubmit} binding={binding}>
 				<FormField type="text" field="firstName" validation="required alphanumeric">First name</FormField>
 				<FormField type="text" field="lastName" validation="required alphanumeric">Last name</FormField>
-				<FormField type="radio" field="gender"  sourcePromise={self.getGender} validation="required">Gender</FormField>
+				<FormField type="radio" field="gender"  sourcePromise={this.getGender} validation="required">Gender</FormField>
                 <FormField type="phone" field="phone" validation="required phone server">Phone</FormField>
 				<FormField type="confirmText" field="email" validation="required email server" >Email</FormField>
 				<FormField type="confirmText" textType="password" field="password" validation="required">Password</FormField>
+				<FormField type="number" field="countOneTimeIosSessions">Count one time Ios sessions</FormField>
+				<FormField type="number" field="countOneTimeAndroidSessions">Count one time Android sessions</FormField>
+				<FormField type="number" field="countOneTimeWebSessions">Count one time Web sessions</FormField>
 			</Form>
 		)
 	}
