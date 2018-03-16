325198cfd6089cdea4eb10b32842de583c0c02ad
Oleg Pikhnenko <mtbstreet91@gmail.com>
2018-03-16 14:24:50 +0600

    Dashboard. School data widget.

---
+++ source/js/module/as_manager/pages/dashboard/dashboard_main_page/actions/school_data_widget_actions.ts
@@ -0,0 +1,53 @@
+import {ServiceList} from "module/core/service_list/service_list";
+
+export const SchoolDataWidgetActions = {
+	getDataForSchoolDataWidget(schoolId: string) {
+		const data = {dataItems: []};
+
+		return this.getSchoolStudentsCount(schoolId)
+			.then((countData) => {
+				data.dataItems.push({name: 'Students', value: String(countData.count)});
+
+				return this.getSchoolFormsCount(schoolId);
+			})
+			.then((countData) => {
+				data.dataItems.push({name: 'Forms', value: String(countData.count)});
+
+				return this.getSchoolHousesCount(schoolId);
+			})
+			.then((countData) => {
+				data.dataItems.push({name: 'Houses', value: String(countData.count)});
+
+				return this.getSchoolTeamsCount(schoolId);
+			})
+			.then((countData) => {
+				data.dataItems.push({name: 'Teams', value: String(countData.count)});
+
+				return data;
+			});
+	},
+	getSchoolStudentsCount(schoolId: string) {
+		return (window.Server as ServiceList).schoolStudentsCount.get({schoolId});
+	},
+	getSchoolFormsCount(schoolId: string) {
+		return (window.Server as ServiceList).schoolForms.get({schoolId}, {filter: {limit: 1000}})
+			.then(forms => {
+				return {count: forms.length};
+			});
+	},
+	getSchoolHousesCount(schoolId: string) {
+		return (window.Server as ServiceList).schoolHouses.get({schoolId}, {filter: {limit: 1000}})
+			.then(houses => {
+				return {count: houses.length};
+			});
+	},
+	getSchoolTeamsCount(schoolId: string) {
+		return (window.Server as ServiceList).teams.get(
+			{schoolId},
+			{filter: {where: {removed:false}, limit: 1000}}
+			)
+			.then(teams => {
+				return {count: teams.length};
+			});
+	}
+};
--- source/js/module/as_manager/pages/dashboard/dashboard_main_page/dashboard_main_page.tsx
+++ source/js/module/as_manager/pages/dashboard/dashboard_main_page/dashboard_main_page.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import * as Morearty from 'morearty'
+import * as Immutable from 'immutable'
 
 import {DashboardCard} from "module/ui/dashboard_components/main_components/dashboard_card/dashboard_card";
 import {DashboardSchoolProfileWidget} from "module/ui/dashboard_components/dashboard_school_profile_widget/dashboard_school_profile_widget";
@@ -7,21 +8,34 @@ import {DashboardSchoolProfileWidget} from "module/ui/dashboard_components/dashb
 import 'styles/ui/dashboard/dashboard_main_page.scss'
 import {DashboardDataWidget} from "module/ui/dashboard_components/dashboard_data_widget/dashboard_data_widget";
 import {DashboardCalendarWidget} from "module/as_manager/pages/dashboard/dashboard_main_page/components/dashboard_calendar_widget/dashboard_calendar_widget";
+import {SchoolDataWidgetActions} from "module/as_manager/pages/dashboard/dashboard_main_page/actions/school_data_widget_actions";
 
 export const DashboardMainPage = (React as any).createClass({
 	mixins: [Morearty.Mixin],
 	propTypes: {
 		activeSchoolId: (React as any).PropTypes.string.isRequired
 	},
+	getDefaultState: function () {
+		return Immutable.fromJS({
+			schoolDataWidget: {
+				data: {
+					dataItems:[
+						{name: 'Students', value: '0'},
+						{name: 'Forms', value: '0'},
+						{name: 'Houses', value: '0'},
+						{name: 'Teams', value: '0'}
+					]
+				}
+			}
+		});
+	},
+	componentWillMount() {
+		SchoolDataWidgetActions.getDataForSchoolDataWidget(this.props.activeSchoolId).then(data => {
+			this.getDefaultBinding().set('schoolDataWidget.data', Immutable.fromJS(data));
+		});
+	},
 	getSchoolDataData() {
-		return {
-			dataItems:[
-				{name: 'Students', value: '113'},
-				{name: 'Forms', value: '15'},
-				{name: 'Houses', value: '29'},
-				{name: 'Teams', value: '9'}
-			]
-		};
+		return this.getDefaultBinding().toJS('schoolDataWidget.data');
 	},
 	getSchoolUsersData() {
 		return {
