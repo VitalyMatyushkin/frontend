cd16d53f92f51c39b904cdb24a3a5716ccc79b23
Alex Povar <a.v.povar@gmail.com>
2018-03-31 14:13:39 +0600

    Dashboard hidden

--- source/js/module/as_manager/head.js
+++ source/js/module/as_manager/head.js
@@ -234,15 +234,15 @@ const Head = React.createClass({
 	 */
 	getMainMenuItemsForSchoolWorker: function() {
 		return [
-			{
-				href			: '/#dashboard/mainPage',
-				icon			: '',
-				name			: 'Dashboard',
-				key				: 'Dashboard',
-				routes			: ['/dashboard', '/dashboard/:subPage'],
-				authorization	: true,
-				verified		: true
-			},
+			// {
+			// 	href			: '/#dashboard/mainPage',
+			// 	icon			: '',
+			// 	name			: 'Dashboard',
+			// 	key				: 'Dashboard',
+			// 	routes			: ['/dashboard', '/dashboard/:subPage'],
+			// 	authorization	: true,
+			// 	verified		: true
+			// },
 			{
 				href			: '/#school_admin/summary',
 				icon			: '',
